52320526b22f487079b207e90cce5c52e3eb231a
Nikita Rudenko <rudenkornk@gmail.com>
2022-05-17 15:01:26 +0300

    Sort projects order in checkout_projects.sh

--- scripts/checkout_projects.sh
+++ scripts/checkout_projects.sh
@@ -13,10 +13,10 @@ fi
 mkdir --parents "$PROJECTS_PATH"
 
 REPOS=()
-REPOS+=("git@github.com:rudenkornk/docker_latex.git")
-REPOS+=("git@github.com:rudenkornk/latex_experiments.git")
 REPOS+=("git@github.com:rudenkornk/docker_ci.git")
+REPOS+=("git@github.com:rudenkornk/docker_latex.git")
 REPOS+=("git@github.com:rudenkornk/group_theory.git")
+REPOS+=("git@github.com:rudenkornk/latex_experiments.git")
 
 for r in ${REPOS[@]}; do
   REPO_NAME=$(echo $r | grep -oP "git@github\.com:.*?/\K.*?(?=\.git)")

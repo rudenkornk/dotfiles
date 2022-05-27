5bbbb2f27d2d069a404cdff56d9a73b45c9b4cbe
Nikita Rudenko <rudenkornk@gmail.com>
2022-05-27 03:07:31 +0300

    Revert "Add github "gh" package"
    This reverts commit d7b73f8cd8ae436d0f8e5c9240b84e7f828d12a7.

--- git/system.sh
+++ git/system.sh
@@ -10,10 +10,3 @@ REPO_PATH=$(realpath "$(dirname "$0")/..")
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends \
   git \
-
-
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt update
-apt install gh
-

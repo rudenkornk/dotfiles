e9935ad72306aeb156d4344dd284f0f8868c95c0
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-28 07:00:06 +0300

    Amend WSL config
    Disable resolv.conf, looks like it is not needed anymore

--- wsl/system.sh
+++ wsl/system.sh
@@ -8,14 +8,17 @@ set -o nounset
 REPO_PATH=$(realpath "$(dirname "$0")/..")
 IS_WSL=$(uname -a | grep --quiet WSL && echo true || echo false)
 
-DNS=8.8.8.8
-RESOLV_CONF=/etc/resolv.conf
-if ! grep $DNS $RESOLV_CONF --quiet; then
-  echo nameserver $DNS >> $RESOLV_CONF
+if [[ $IS_WSL != "true" ]]; then
+  exit
 fi
 
 WSL_CONF=/etc/wsl.conf
-if [[ $IS_WSL == "true" && ((-f $WSL_CONF && ! -s $WSL_CONF) || ! -f $WSL_CONF) ]]; then
+if [[ ((-f $WSL_CONF && ! -s $WSL_CONF) || ! -f $WSL_CONF) ]]; then
   ln --symbolic --force "$REPO_PATH/wsl/wsl.conf" $WSL_CONF
 fi
 
+#DNS=8.8.8.8
+#RESOLV_CONF=/etc/resolv.conf
+#if ! grep $DNS $RESOLV_CONF --quiet; then
+#  echo nameserver $DNS >> $RESOLV_CONF
+#fi

1eda79fd458a33ce5e5a90ff96bfda281652c6aa
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-28 06:58:33 +0300

    Add keyboard_layouts config updatable

--- keyboard_layouts/system.sh
+++ keyboard_layouts/system.sh
@@ -7,8 +7,14 @@ set -o nounset
 
 REPO_PATH=$(realpath "$(dirname "$0")/..")
 
-if [[ -d /usr/share/X11/xkb/symbols ]]; then
-  ln -s "$REPO_PATH/keyboard_layouts/rnk" /usr/share/X11/xkb/symbols/rnk
+if [[ -d /usr/share/X11/xkb ]]; then
+  ln --symbolic --force "$REPO_PATH/keyboard_layouts/rnk" /usr/share/X11/xkb/symbols/rnk
+  target_file=/usr/share/X11/xkb/rules/evdev.xml
+  insert_before=$(grep --line-number "</layoutList>" $target_file \
+    | cut --delimiter ":" --fields 1)
+  begin="<!-- dotfiles begin -->"
+  end="<!-- dotfiles end -->"
+  "$REPO_PATH/scripts/insert_text.sh" "$REPO_PATH/keyboard_layouts/evdev.xml" $target_file "$begin" "$end" $insert_before
 fi
 # Insert contents of evdev.xml into /usr/share/X11/xkb/rules/evdev.xml into appropriate place
 # sudo dpkg-reconfigure xkb-data

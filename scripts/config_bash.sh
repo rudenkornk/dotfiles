434980e7f0ddc3e556e860247be1c5ccfd359236
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-30 15:34:32 +0300

    Revert "Change entries in .bashrc with includes"

--- scripts/config_bash.sh
+++ scripts/config_bash.sh
@@ -10,9 +10,7 @@ REPO_PATH=$(realpath "$(dirname "$0")/..")
 bash_source="$1"
 component=$(basename $(realpath $bash_source))
 
-echo "source $bash_source/bashrc" > source_bashrc
 begin="# --- dotfiles $component begin --- #"
 end="# --- dotfiles $component end --- #"
-"$REPO_PATH/scripts/insert_text.sh" source_bashrc ~/.bashrc "$begin" "$end"
-rm source_bashrc
+"$REPO_PATH/scripts/insert_text.sh" "$bash_source/bashrc" ~/.bashrc "$begin" "$end"
 

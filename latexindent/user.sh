2808babbc60bf0499ebdcdec54082f4adf94baee
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-30 16:15:53 +0300

    Make latexindent config updatable

--- latexindent/user.sh
+++ latexindent/user.sh
@@ -11,12 +11,15 @@ UBUNTU_VERSION=$(lsb_release -d | grep --only-matching --perl-regexp "[\d\.]+")
 if ! { echo "22.04"; echo "$UBUNTU_VERSION"; } | \
   sort --version-sort --check &> /dev/null; then
   wget https://github.com/cmhughes/latexindent.pl/releases/download/V3.17.2/latexindent.zip
-  unzip latexindent.zip
-  mv "latexindent/latexindent.pl" ~/.local/bin/
-  mv "latexindent/defaultSettings.yaml" ~/.local/bin/
-  mv "latexindent/LatexIndent" ~/.local/bin/
-  ln --symbolic ~/.local/bin/latexindent.pl ~/.local/bin/latexindent
+  unzip latexindent.zip -d tmp
+  mv --force "tmp/latexindent/latexindent.pl" ~/.local/bin/
+  mv --force "tmp/latexindent/defaultSettings.yaml" ~/.local/bin/
+  if [[ -d ~/.local/bin/LatexIndent ]]; then
+    rm --recursive --force ~/.local/bin/LatexIndent
+  fi
+  mv --force "tmp/latexindent/LatexIndent" ~/.local/bin/
+  ln --symbolic --force ~/.local/bin/latexindent.pl ~/.local/bin/latexindent
   rm "latexindent.zip"
-  rm --recursive --force "latexindent"
+  rm --recursive --force "tmp"
 fi
 

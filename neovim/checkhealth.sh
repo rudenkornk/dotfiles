66f44291e182eb7c33c05e6dbe9b7ea79e737e88
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-01 20:48:32 +0300

    Amend neovim checkhealth script

--- neovim/checkhealth.sh
+++ neovim/checkhealth.sh
@@ -3,12 +3,20 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
+set -o xtrace
 
 REPO_PATH=$(realpath "$(dirname "$0")/..")
 SELF_PATH=$(realpath "$(dirname "$0")")
 
 export PATH="$HOME/.local/bin:$PATH"
-nvim --headless -c "checkhealth" -c "w health" -c quitall
 
-rm health
+for component in \
+                 coc \
+                 nvim-treesitter \
+                 provider \
+                 ; do
+  nvim --headless -c "checkhealth $component" -c "w ${component}_health" -c quitall
+  ! grep --quiet ERROR ${component}_health
+  rm ${component}_health
+done
+

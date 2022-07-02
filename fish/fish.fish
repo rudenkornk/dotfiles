da5e4a99f525115cc9ad49cf144075455934a209
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-02 11:38:39 +0300

    Amend ssh management in fish

--- fish/fish.fish
+++ fish/fish.fish
@@ -6,12 +6,44 @@ else
   contains "$local_bin" $fish_user_paths; or set -Ua fish_user_paths "$local_bin"
 end
 
-if not set -q SSH_AUTH_SOCK && [ -d ~/.ssh ] && [ (find ~/.ssh -name "*id_rsa" | wc -l) -gt 0 ]
-  eval (ssh-agent -c) &> /dev/null
-  ssh-add ~/.ssh/*id_rsa &> /dev/null
-end
-
 if uname --all | grep --quiet WSL
   set --erase DISPLAY
+end
+
+
+# Credits: https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
+setenv SSH_ENV $HOME/.ssh/environment
+function start_agent
+  echo "Initializing new SSH agent ..."
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  echo "succeeded"
+  chmod 600 $SSH_ENV
+  . $SSH_ENV > /dev/null
+  ssh-add "~/.ssh/*_id_rsa"
+end
+function test_identities
+  ssh-add -l | grep "The agent has no identities" > /dev/null
+  if [ $status -eq 0 ]
+    ssh-add "~/.ssh/*_id_rsa"
+    if [ $status -eq 2 ]
+      start_agent
+    end
+  end
+end
+if [ -n "$SSH_AGENT_PID" ]
+  ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  end
+else
+  if [ -f $SSH_ENV ]
+    . $SSH_ENV > /dev/null
+  end
+  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  else
+    start_agent
+  end
 end
 

4987c9928012ccd0aed146af40f2a36dc89ea930
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-21 18:34:12 +0300

    Add ssh component

---
+++ ssh/ssh.fish
@@ -0,0 +1,41 @@
+# Credits: https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
+setenv SSH_ENV $HOME/.ssh/environment
+set -l keys ~/.ssh/*_id_rsa
+function start_agent
+  echo "Initializing new SSH agent ..."
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  echo "succeeded"
+  chmod 600 $SSH_ENV
+  . $SSH_ENV > /dev/null
+  if test (count $keys) -gt 0
+    ssh-add ~/.ssh/*_id_rsa
+  end
+end
+function test_identities
+  ssh-add -l | grep "The agent has no identities" > /dev/null
+  if [ $status -eq 0 ]
+    if test (count $keys) -gt 0
+      ssh-add ~/.ssh/*_id_rsa
+    end
+    if [ $status -eq 2 ]
+      start_agent
+    end
+  end
+end
+if [ -n "$SSH_AGENT_PID" ]
+  ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  end
+else
+  if [ -f $SSH_ENV ]
+    . $SSH_ENV > /dev/null
+  end
+  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  else
+    start_agent
+  end
+end
+
---
+++ ssh/user.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+REPO_PATH=$(realpath "$(dirname "$0")/..")
+SELF_PATH=$(realpath "$(dirname "$0")")
+
+mkdir --parents ~/.ssh
+touch ~/.ssh/environment
+
+"$REPO_PATH/scripts/config_fish.sh" "$SELF_PATH"
+

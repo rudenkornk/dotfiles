cc5c613177c9950ce13edb14f2faeae580fbb6cb
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-26 02:05:35 +0300

    Fix missing fish configuration for rust

--- rust/user.sh
+++ rust/user.sh
@@ -6,9 +6,12 @@ set -o nounset
 #set -o xtrace
 
 REPO_PATH=$(realpath "$(dirname "$0")/..")
+SELF_PATH=$(realpath "$(dirname "$0")")
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs &> rustup.rs
 chmod +x rustup.rs
 ./rustup.rs -y
 rm rustup.rs
+
+"$REPO_PATH/scripts/config_fish.sh" "$SELF_PATH"
 

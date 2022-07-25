0a849c295e6f7c20bcec466a357245032ccf08a9
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-25 15:16:56 +0300

    Add language info to neovim status line

--- neovim/nvchad/plugins/ui.lua
+++ neovim/nvchad/plugins/ui.lua
@@ -4,11 +4,17 @@ return {
       local st_modules = require "nvchad_ui.statusline.modules"
       local paste_status = ""
       if vim.o.paste then
-         paste_status = "%#St_file_info#PASTE"
+         paste_status = "%#St_file_info# paste"
+      end
+      local lang = "%#St_file_info#"
+      if vim.o.iminsert==1 then
+        lang = lang .. "ru"
+      else
+        lang = lang .. "en"
       end
       return {
          mode = function ()
-            return st_modules.mode() .. paste_status
+            return st_modules.mode() .. lang .. paste_status
          end
       }
     end,

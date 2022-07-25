f47e5f6ee1b48d40f928bb7e588717eaccef2c79
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-25 16:01:33 +0300

    Rewrite some config parts with lua

--- neovim/nvchad/init.lua
+++ neovim/nvchad/init.lua
@@ -1,26 +1,24 @@
 -- Support russian with specific keyboard layouts
-vim.cmd([[ set keymap=rnk-russian-qwerty ]])
-vim.cmd([[ set iminsert=0 ]])
-vim.cmd([[ set imsearch=-1 ]])
+vim.o.keymap = "rnk-russian-qwerty"
+vim.o.iminsert = 0
+vim.o.imsearch = -1
 
 -- Disable formatting in insert mode
 -- Unfortunatelly this breaks autocomplete with tab, so it is not permanently
 -- enabled
 -- vim.cmd([[ set paste ]])
 -- Instead use pastetoggle workaround
-vim.cmd([[ set pastetoggle=<C-q> ]])
+vim.o.pastetoggle = "<C-q>"
 
 --vim.cmd([[ set wildmode=longest:list,full ]])
 
-vim.cmd([[ set nobackup ]])
-vim.cmd([[ set nowritebackup ]])
+vim.g.nobackup = true
+vim.g.nowritebackup = true
 
 -- Setup gdb
 vim.cmd([[ packadd termdebug ]])
-vim.cmd([[ let g:termdebug_wide = 163 ]])
+vim.g.termdebug_wide = 163
 
-vim.cmd([[ set tabstop=2 ]])
-vim.cmd([[ set expandtab ]])
 vim.cmd([[ set nrformats+=alpha ]])
 vim.cmd([[ highlight RedundantSpacesAndTabs ctermbg=100 guibg=#b5942e ]])
 vim.cmd([[ match RedundantSpacesAndTabs /\(\s\+$\|\t\+\)/ ]])

19381ff3144f1e297ee1d479df876c4a3875280e
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-25 18:06:31 +0300

    Refactor mappings

--- neovim/nvchad/mappings.lua
+++ neovim/nvchad/mappings.lua
@@ -8,15 +8,28 @@ M.disabled = {
     ["<C-l>"] = "", -- Unmap pane switch. This will be managed by Navigator.
     ["<TAB>"] = "", -- Unmap buffer cycling. This interferes with <C-i> behaviour.
     ["<S-b>"] = "", -- Unmap creating new buffer. This interferes with default <S-b> behaviour.
-    ["<leader>tp"] = "", -- Unmap tabs cycling. Instead, assign buffers cycling to these keys.
-    ["<leader>tn"] = "", -- Unmap tabs cycling. Instead, assign buffers cycling to these keys.
   },
 }
 
-M.custom = {
+M.tabufline = {
   n = {
-    ["<C-g>"] = { "a<C-^><ESC>", "וּ Toggle layout" },
-    ["<leader>vc"] = { "<CMD>%s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", " Clear extra spaces" },
+    ["<leader>tp"] = { "<CMD> Tbufnext <CR>", "  goto next buffer" },
+    ["<leader>tn"] = { "<CMD> Tbufprev <CR>", "  goto prev buffer" },
+  },
+}
+
+M.navigator = {
+  n = {
+    ["<C-w>h"] = { "<CMD>NavigatorLeft<CR>", " Left pane" },
+    ["<C-w>j"] = { "<CMD>NavigatorDown<CR>", " Lower pane" },
+    ["<C-w>k"] = { "<CMD>NavigatorUp<CR>", " Upper pane" },
+    ["<C-w>l"] = { "<CMD>NavigatorRight<CR>", " Right pane" },
+    ["<C-w>p"] = { "<CMD>NavigatoPrevious<CR>", " Previous pane" },
+  },
+}
+
+M.termdebug = {
+  n = {
     ["<leader>db"] = { ":Break<CR>", " Set breakpoint" },
     ["<leader>dr"] = { ":call TermDebugSendCommand('run')<CR>", " Run" },
     ["<leader>dn"] = { ":call TermDebugSendCommand('next')<CR>", " Step over" },
@@ -26,23 +39,32 @@ M.custom = {
     ["<leader>du"] = { ":call TermDebugSendCommand('up')<CR>", " Up stack" },
     ["<leader>dd"] = { ":call TermDebugSendCommand('down')<CR>", " Down stack" },
     ["<leader>dt"] = { ":call TermDebugSendCommand('backtrace')<CR>", " Show backtrace" },
+  },
+}
+
+M.illuminate = {
+  n = {
     ["<A-n>"] = { "<CMD>lua require('illuminate').next_reference{wrap=true}<CR>", " Next reference" },
     ["<A-p>"] = { "<CMD>lua require('illuminate').next_reference{reverse=true,wrap=true}<CR>", " Previous reference" },
-
-    ["<leader>tp"] = { "<CMD> Tbufnext <CR>", "  goto next buffer" },
-    ["<leader>tn"] = { "<CMD> Tbufprev <CR>", "  goto prev buffer" },
-
-    ["<C-w>h"] = { "<CMD>NavigatorLeft<CR>", " Left pane" },
-    ["<C-w>j"] = { "<CMD>NavigatorDown<CR>", " Lower pane" },
-    ["<C-w>k"] = { "<CMD>NavigatorUp<CR>", " Upper pane" },
-    ["<C-w>l"] = { "<CMD>NavigatorRight<CR>", " Right pane" },
-    ["<C-w>p"] = { "<CMD>NavigatoPrevious<CR>", " Previous pane" },
   },
-
   i = {
-    ["<C-g>"] = { "<C-^>", "וּ Toggle layout" },
     ["<A-n>"] = { "<cmd>lua require('illuminate').next_reference{wrap=true}<CR>", " Next reference" },
     ["<A-p>"] = { "<cmd>lua require('illuminate').next_reference{reverse=true,wrap=true}<CR>", " Previous reference" },
+  },
+}
+
+M.layout = {
+  n = {
+    ["<C-g>"] = { "a<C-^><ESC>", "וּ Toggle layout" },
+  },
+  i = {
+    ["<C-g>"] = { "<C-^>", "וּ Toggle layout" },
+  },
+}
+
+M.utils = {
+  n = {
+    ["<leader>vc"] = { "<CMD>%s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", " Clear extra spaces" },
   },
   x = {
     ["<leader>vc"] = { "<CMD>s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", " Clear extra spaces" },

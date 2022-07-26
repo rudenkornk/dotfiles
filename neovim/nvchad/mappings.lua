e91f5a91f5943978c620736fb9f346d25e8b5498
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-26 08:26:18 +0300

    [neovim] Add 2 spaces between icon and mapping description as siduck recommends

--- neovim/nvchad/mappings.lua
+++ neovim/nvchad/mappings.lua
@@ -13,12 +13,12 @@ M.disabled = {
 
 M.utils = {
   n = {
-    ["<leader>vc"] = { "<CMD>%s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", " Clear extra spaces" },
+    ["<leader>vc"] = { "<CMD>%s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", "  Clear extra spaces" },
   },
   x = {
-    ["<leader>vc"] = { "<CMD>s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", " Clear extra spaces" },
-    ["<leader>vt"] = { "<CMD>s/\\([^ ]\\)\\s\\+/\\1 /g | noh | '<,'>Tabularize /\\s\\+/l0<CR>", "璘 Tabularize" },
-    ["<leader>vs"] = { ":sort<CR>", " Sort" },
+    ["<leader>vc"] = { "<CMD>s/\\([^ ]\\)\\s\\+/\\1 /g | noh <CR>", "  Clear extra spaces" },
+    ["<leader>vt"] = { "<CMD>s/\\([^ ]\\)\\s\\+/\\1 /g | noh | '<,'>Tabularize /\\s\\+/l0<CR>", "璘  Tabularize" },
+    ["<leader>vs"] = { ":sort<CR>", "  Sort" },
   },
 }
 vim.o.pastetoggle = "<C-q>"
@@ -26,21 +26,21 @@ vim.cmd([[ cnoremap <C-a> <HOME>]])
 
 M.layout = {
   n = {
-    ["<C-g>"] = { "a<C-^><ESC>", "וּ Toggle layout" },
+    ["<C-g>"] = { "a<C-^><ESC>", "וּ  Toggle layout" },
   },
   i = {
-    ["<C-g>"] = { "<C-^>", "וּ Toggle layout" },
+    ["<C-g>"] = { "<C-^>", "וּ  Toggle layout" },
   },
 }
 
 M.trouble = {
   n = {
-    ["<leader>q"] = { "<CMD>TroubleToggle<CR>", " diagnostic list" },
-    ["<leader>xw"] = { "<CMD>TroubleToggle workspace_diagnostics<CR>", " workspace diagnostic list" },
-    ["<leader>xd"] = { "<CMD>TroubleToggle document_diagnostics<CR>", " document diagnostic list" },
-    ["<leader>xq"] = { "<CMD>TroubleToggle quickfix<CR>", " quickfix list" },
-    ["<leader>xl"] = { "<CMD>TroubleToggle loclist<CR>", " location list" },
-    ["gr"] = { "<CMD>TroubleToggle lsp_references<CR>", " lsp references" },
+    ["<leader>q"] = { "<CMD>TroubleToggle<CR>", "  diagnostic list" },
+    ["<leader>xw"] = { "<CMD>TroubleToggle workspace_diagnostics<CR>", "  workspace diagnostic list" },
+    ["<leader>xd"] = { "<CMD>TroubleToggle document_diagnostics<CR>", "  document diagnostic list" },
+    ["<leader>xq"] = { "<CMD>TroubleToggle quickfix<CR>", "  quickfix list" },
+    ["<leader>xl"] = { "<CMD>TroubleToggle loclist<CR>", "  location list" },
+    ["gr"] = { "<CMD>TroubleToggle lsp_references<CR>", "  lsp references" },
   },
 }
 
@@ -50,70 +50,70 @@ M.lspconfig = {
       function()
         vim.diagnostic.goto_next()
       end,
-      " goto_next",
+      "  goto_next",
     },
   },
 }
 
 M.tabufline = {
   n = {
-    ["<leader>tp"] = { "<CMD> Tbufnext <CR>", " goto next buffer" },
-    ["<leader>tn"] = { "<CMD> Tbufprev <CR>", " goto prev buffer" },
+    ["<leader>tp"] = { "<CMD> Tbufnext <CR>", "  goto next buffer" },
+    ["<leader>tn"] = { "<CMD> Tbufprev <CR>", "  goto prev buffer" },
   },
 }
 
 M.navigator = {
   n = {
-    ["<C-h>"] = { "<CMD>NavigatorLeft<CR>", " Left pane" },
-    ["<C-j>"] = { "<CMD>NavigatorDown<CR>", " Lower pane" },
-    ["<C-k>"] = { "<CMD>NavigatorUp<CR>", " Upper pane" },
-    ["<C-l>"] = { "<CMD>NavigatorRight<CR>", " Right pane" },
+    ["<C-h>"] = { "<CMD>NavigatorLeft<CR>", "  Left pane" },
+    ["<C-j>"] = { "<CMD>NavigatorDown<CR>", "  Lower pane" },
+    ["<C-k>"] = { "<CMD>NavigatorUp<CR>", "  Upper pane" },
+    ["<C-l>"] = { "<CMD>NavigatorRight<CR>", "  Right pane" },
   },
   t = {
-    ["<C-h>"] = { "<CMD>NavigatorLeft<CR>", " Left pane" },
-    ["<C-j>"] = { "<CMD>NavigatorDown<CR>", " Lower pane" },
-    ["<C-k>"] = { "<CMD>NavigatorUp<CR>", " Upper pane" },
-    ["<C-l>"] = { "<CMD>NavigatorRight<CR>", " Right pane" },
+    ["<C-h>"] = { "<CMD>NavigatorLeft<CR>", "  Left pane" },
+    ["<C-j>"] = { "<CMD>NavigatorDown<CR>", "  Lower pane" },
+    ["<C-k>"] = { "<CMD>NavigatorUp<CR>", "  Upper pane" },
+    ["<C-l>"] = { "<CMD>NavigatorRight<CR>", "  Right pane" },
   },
 }
 
 M.termdebug = {
   n = {
-    ["<A-g>"] = { "<CMD>Termdebug<CR>i", "ﴫ Run gdb" },
-    ["<A-r>"] = { "<CMD>call TermDebugSendCommand('run')<CR>", " Run" },
-    ["<A-b>"] = { "<CMD>Break<CR>", " Set breakpoint" },
-    ["<A-j>"] = { "<CMD>call TermDebugSendCommand('next')<CR>", " Step over" },
-    ["<A-k>"] = { "<CMD>call TermDebugSendCommand('step')<CR>", " Step into" },
-    ["<A-o>"] = { "<CMD>call TermDebugSendCommand('finish')<CR>", " Step out" },
-    ["<A-c>"] = { "<CMD>call TermDebugSendCommand('continue')<CR>", "懶 Continue" },
-    ["<A-u>"] = { "<CMD>call TermDebugSendCommand('up')<CR>", " Up stack" },
-    ["<A-d>"] = { "<CMD>call TermDebugSendCommand('down')<CR>", " Down stack" },
+    ["<A-g>"] = { "<CMD>Termdebug<CR>i", "ﴫ  Run gdb" },
+    ["<A-r>"] = { "<CMD>call TermDebugSendCommand('run')<CR>", "  Run" },
+    ["<A-b>"] = { "<CMD>Break<CR>", "  Set breakpoint" },
+    ["<A-j>"] = { "<CMD>call TermDebugSendCommand('next')<CR>", "  Step over" },
+    ["<A-k>"] = { "<CMD>call TermDebugSendCommand('step')<CR>", "  Step into" },
+    ["<A-o>"] = { "<CMD>call TermDebugSendCommand('finish')<CR>", "  Step out" },
+    ["<A-c>"] = { "<CMD>call TermDebugSendCommand('continue')<CR>", "懶  Continue" },
+    ["<A-u>"] = { "<CMD>call TermDebugSendCommand('up')<CR>", "  Up stack" },
+    ["<A-d>"] = { "<CMD>call TermDebugSendCommand('down')<CR>", "  Down stack" },
   },
   t = {
-    ["<A-r>"] = { "<CMD>call TermDebugSendCommand('run')<CR>", " Run" },
-    ["<A-j>"] = { "<CMD>call TermDebugSendCommand('next')<CR>", " Step over" },
-    ["<A-k>"] = { "<CMD>call TermDebugSendCommand('step')<CR>", " Step into" },
-    ["<A-o>"] = { "<CMD>call TermDebugSendCommand('finish')<CR>", " Step out" },
-    ["<A-c>"] = { "<CMD>call TermDebugSendCommand('continue')<CR>", "懶 Continue" },
-    ["<A-u>"] = { "<CMD>call TermDebugSendCommand('up')<CR>", " Up stack" },
-    ["<A-d>"] = { "<CMD>call TermDebugSendCommand('down')<CR>", " Down stack" },
-    ["<ESC>"] = { termcodes("<C-\\><C-N>"), "   escape terminal mode" },
+    ["<A-r>"] = { "<CMD>call TermDebugSendCommand('run')<CR>", "  Run" },
+    ["<A-j>"] = { "<CMD>call TermDebugSendCommand('next')<CR>", "  Step over" },
+    ["<A-k>"] = { "<CMD>call TermDebugSendCommand('step')<CR>", "  Step into" },
+    ["<A-o>"] = { "<CMD>call TermDebugSendCommand('finish')<CR>", "  Step out" },
+    ["<A-c>"] = { "<CMD>call TermDebugSendCommand('continue')<CR>", "懶  Continue" },
+    ["<A-u>"] = { "<CMD>call TermDebugSendCommand('up')<CR>", "  Up stack" },
+    ["<A-d>"] = { "<CMD>call TermDebugSendCommand('down')<CR>", "  Down stack" },
+    ["<ESC>"] = { termcodes("<C-\\><C-N>"), "  escape terminal mode" },
   },
 }
 
 M.illuminate = {
   n = {
-    ["<A-n>"] = { "<CMD>lua require('illuminate').next_reference{wrap=true}<CR>", " Next reference" },
+    ["<A-n>"] = { "<CMD>lua require('illuminate').next_reference{wrap=true}<CR>", "  Next reference" },
     ["<A-p>"] = {
       "<CMD>lua require('illuminate').next_reference{reverse=true,wrap=true}<CR>",
-      " Previous reference",
+      "  Previous reference",
     },
   },
   i = {
-    ["<A-n>"] = { "<cmd>lua require('illuminate').next_reference{wrap=true}<CR>", " Next reference" },
+    ["<A-n>"] = { "<cmd>lua require('illuminate').next_reference{wrap=true}<CR>", "  Next reference" },
     ["<A-p>"] = {
       "<cmd>lua require('illuminate').next_reference{reverse=true,wrap=true}<CR>",
-      " Previous reference",
+      "  Previous reference",
     },
   },
 }

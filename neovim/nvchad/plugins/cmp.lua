b78e13289b9df93a197fb4db05b886120d832118
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-27 22:24:43 +0300

    [neovim] Add default options to cmp config They will be modified later

--- neovim/nvchad/plugins/cmp.lua
+++ neovim/nvchad/plugins/cmp.lua
@@ -1,4 +1,5 @@
 return function()
+  local cmp = require("cmp")
   return {
     sources = {
       { name = "buffer" },
@@ -9,6 +10,30 @@ return function()
       { name = "path" },
       { name = "rg" },
       { name = "tmux" },
+    },
+    formatting = {
+      format = function(entry, vim_item)
+        local icons = require("nvchad_ui.icons").lspkind
+        local kind = vim_item.kind
+        local icon = icons[kind]
+        vim_item.kind = string.format("%s %s", icon, kind)
+        return vim_item
+      end,
+    },
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        -- cmp.config.compare.scopes,
+        cmp.config.compare.score,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
     },
   }
 end

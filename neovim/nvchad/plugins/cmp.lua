c1bb85a92b38e322348552b4db3abd6082ad79f5
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-27 05:16:00 +0300

    [neovim] Wrap cmp config under a function

--- neovim/nvchad/plugins/cmp.lua
+++ neovim/nvchad/plugins/cmp.lua
@@ -1,12 +1,14 @@
-return {
-  sources = {
-    { name = "buffer" },
-    { name = "git" },
-    { name = "luasnip" },
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "path" },
-    { name = "rg" },
-    { name = "tmux" },
-  },
-}
+return function()
+  return {
+    sources = {
+      { name = "buffer" },
+      { name = "git" },
+      { name = "luasnip" },
+      { name = "nvim_lsp" },
+      { name = "nvim_lua" },
+      { name = "path" },
+      { name = "rg" },
+      { name = "tmux" },
+    },
+  }
+end

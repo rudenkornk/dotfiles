211407b21b97f89489e3dd5a1008d6f98bc68459
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-27 22:22:55 +0300

    [neovim] Refactor highlights in neovim

--- neovim/nvchad/init.lua
+++ neovim/nvchad/init.lua
@@ -20,7 +20,6 @@ vim.cmd([[ packadd termdebug ]])
 vim.g.termdebug_wide = 163
 
 vim.cmd([[ set nrformats+=alpha ]])
-vim.cmd([[ highlight RedundantSpacesAndTabs ctermbg=100 guibg=#b5942e ]])
 vim.cmd([[ match RedundantSpacesAndTabs /\(\s\+$\|\t\+\)/ ]])
 
 -- load your globals, autocmds here or anything .__.

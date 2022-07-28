412722fee664d196af14dbff8edde7889e6d1937
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-28 07:22:32 +0300

    [neovim] Remove non-working illuminate config part

--- neovim/nvchad/plugins/illuminate.lua
+++ neovim/nvchad/plugins/illuminate.lua
@@ -1,12 +1,3 @@
 return {
   module = "illuminate",
-  setup = function()
-    -- does not work :(
-    vim.g.Illuminate_highlightUnderCursor = 0
-    local autocmd = vim.api.nvim_create_autocmd
-    autocmd("VimEnter", {
-      pattern = "*",
-      command = "hi illuminatedWord cterm=underline gui=underline",
-    })
-  end,
 }

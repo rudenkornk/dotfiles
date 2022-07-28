8a7e9c9d9e30146e26de2b249698210c3facdbee
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-29 01:14:11 +0300

    [neovim] Refactor colors

--- neovim/nvchad/ui.lua
+++ neovim/nvchad/ui.lua
@@ -1,72 +1,31 @@
 local themes = {
-  ["dark"] = "radium",
-  ["light"] = "catppuccin_latte",
+  dark = "radium",
+  light = "catppuccin_latte",
 }
 local kind = "dark"
 local theme_name = themes[kind]
-
-local colors = {
-  dark = {
-    buffer_cmp = { fg = "green" },
-    cmdline_cmp = { fg = "purple" },
-    copilot_cmp = { fg = "blue" },
-    focused_symbol = {},
-    git_cmp = { fg = "green" },
-    illuminate = { fg = "cyan", bg = "one_bg2" },
-    lsp_cmp = { fg = "yellow" },
-    luasnip_cmp = { fg = "red" },
-    nvim_lua_cmp = { fg = "yellow" },
-    path_cmp = { fg = "white" },
-    rg_cmp = { fg = "green" },
-    spaces = { bg = "grey" },
-    tabnine_cmp = { fg = "dark_purple" },
-    tmux_tmp = { fg = "green" },
-  },
-  light = {
-    buffer_cmp = { fg = "green" },
-    cmdline_cmp = { fg = "purple" },
-    copilot_cmp = { fg = "blue" },
-    focused_symbol = {},
-    git_cmp = { fg = "green" },
-    illuminate = { fg = "cyan", bg = "one_bg2" },
-    lsp_cmp = { fg = "yellow" },
-    luasnip_cmp = { fg = "red" },
-    nvim_lua_cmp = { fg = "yellow" },
-    path_cmp = { fg = "white" },
-    rg_cmp = { fg = "green" },
-    spaces = { bg = "grey" },
-    tabnine_cmp = { fg = "dark_purple" },
-    tmux_tmp = { fg = "green" },
-  },
-}
-
-local function get_colors(component)
-  return colors[theme_name] and colors[theme_name][component] or colors[kind][component]
-end
-
 
 return {
   theme = theme_name,
   theme_toggle = { themes.dark, themes.light },
   hl_override = {
-    LspReferenceRead = get_colors("illuminate"),
-    LspReferenceText = get_colors("illuminate"),
-    LspReferenceWrite = get_colors("illuminate"),
+    LspReferenceRead = { fg = "cyan", bg = "one_bg2" },
+    LspReferenceText = { fg = "cyan", bg = "one_bg2" },
+    LspReferenceWrite = { fg = "cyan", bg = "one_bg2" },
   },
   hl_add = {
-    FocusedSymbol = get_colors("focused_symbol"),
-    CmpItemMenuBuffer = get_colors("buffer_cmp"),
-    CmpItemMenuCmdLine = get_colors("cmdline_cmp"),
-    CmpItemMenuCopilot = get_colors("copilot_cmp"),
-    CmpItemMenuGit = get_colors("git_cmp"),
-    CmpItemMenuLSP = get_colors("lsp_cmp"),
-    CmpItemMenuLuasnip = get_colors("luasnip_cmp"),
-    CmpItemMenuNeovimLua = get_colors("nvim_lua_cmp"),
-    CmpItemMenuPath = get_colors("path_cmp"),
-    CmpItemMenuRipGrep = get_colors("rg_cmp"),
-    CmpItemMenuTabnine = get_colors("tabnine_cmp"),
-    CmpItemMenuTmux = get_colors("tmux_tmp"),
-    RedundantSpacesAndTabs = get_colors("spaces"),
+    CmpItemMenuBuffer = { link = "CmpItemKindText" },
+    CmpItemMenuCmdLine = { link = "CmpItemKindVariable" },
+    CmpItemMenuCopilot = { fg = "blue" },
+    CmpItemMenuGit = { link = "CmpItemKindText" },
+    CmpItemMenuLSP = { link = "CmpItemKindFunction" },
+    CmpItemMenuLuasnip = { link = "CmpItemKindSnippet" },
+    CmpItemMenuNeovimLua = { link = "CmpItemKindFunction" },
+    CmpItemMenuPath = { fg = "CmpItemKindFolder" },
+    CmpItemMenuRipGrep = { link = "CmpItemKindText" },
+    CmpItemMenuTabnine = { fg = "dark_purple" },
+    CmpItemMenuTmux = { link = "CmpItemKindText" },
+    FocusedSymbol = {},
+    RedundantSpacesAndTabs = { bg = "grey" },
   },
-  transparency = false,
 }

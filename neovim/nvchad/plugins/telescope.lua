2d71d890cc011402b9dc4aab5e26600ba36afa62
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-28 06:09:15 +0300

    [neovim] Fix wrong mappings for telescope scroll in neovim

--- neovim/nvchad/plugins/telescope.lua
+++ neovim/nvchad/plugins/telescope.lua
@@ -2,8 +2,8 @@ return {
   defaults = {
     mappings = {
       i = {
-        ["<C-f>"] = "results_scrolling_up",
-        ["<C-b>"] = "results_scrolling_down",
+        ["<C-f>"] = "results_scrolling_down",
+        ["<C-b>"] = "results_scrolling_up",
         ["<C-a>"] = { "<ESC>^Wi", type = "command" },
       },
     },

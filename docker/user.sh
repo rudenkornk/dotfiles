7ca8cbb75829e528c53d5df2f4ad319a172f4e63
Nikita Rudenko <rudenkornk@gmail.com>
2022-08-15 12:07:03 +0300

    [docker] Amend config generation

--- docker/user.sh
+++ docker/user.sh
@@ -11,11 +11,12 @@ SELF_PATH=$(realpath "$(dirname "$0")")
 mkdir --parents ~/.docker
 
 config=~/.docker/config.json
-if [[ ! -f $config ]]; then
+if [[ ! -f $config || ! -s $config ]]; then
   echo "{}" > $config
 fi
+
 # Do not create symbolic because it might be populated with docker credentials
-cat $config | jq '.detachKeys="ctrl-z"' | sponge $config
+jq -s '.[0] * .[1]' "$SELF_PATH/config.json" "$config" | sponge $config
 
 mkdir --parents ~/.config/fish/completions
 wget https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish

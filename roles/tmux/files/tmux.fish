a9bc8fbfad7eb84795546324fbd2bced8dc82df9
Nikita Rudenko <rudenkornk@gmail.com>
2023-01-08 04:23:13 +0300

    [tmux] Fix tmux color scheme

--- roles/tmux/files/tmux.fish
+++ roles/tmux/files/tmux.fish
@@ -1,5 +1,5 @@
 if status is-interactive
 and not set -q TMUX
-  not tmux ls &> /dev/null; and exec tmux
-  tmux ls | grep --quiet --invert-match "(attached)"; and exec tmux a
+  not tmux ls &> /dev/null; and exec tmux -2 -u
+  tmux ls | grep --quiet --invert-match "(attached)"; and exec tmux -2 -u a
 end

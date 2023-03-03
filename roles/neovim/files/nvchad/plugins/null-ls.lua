8ec88f8e7185d57512f3df094b07c324b9cc673c
Nikita Rudenko <rudenkornk@gmail.com>
2023-03-03 13:34:18 +0300

    [neovim] Add more linters

--- roles/neovim/files/nvchad/plugins/null-ls.lua
+++ roles/neovim/files/nvchad/plugins/null-ls.lua
@@ -7,14 +7,24 @@ return {
     local null_ls = require("null-ls")
 
     local sources = {
+      -- generic
+      null_ls.builtins.diagnostics.write_good,
+
+      -- Ansible
+      null_ls.builtins.diagnostics.ansiblelint,
+
       -- C++
       null_ls.builtins.formatting.clang_format,
 
       -- CMake
       null_ls.builtins.formatting.cmake_format,
+      null_ls.builtins.diagnostics.cmake_lint,
 
       -- dockerfile
       null_ls.builtins.diagnostics.hadolint,
+
+      -- .editorconfig
+      null_ls.builtins.diagnostics.editorconfig_checker,
 
       -- fish
       null_ls.builtins.diagnostics.fish,
@@ -22,6 +32,7 @@ return {
 
       -- git
       null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.diagnostics.gitlint,
 
       -- groovy
       null_ls.builtins.formatting.npm_groovy_lint,
@@ -34,6 +45,8 @@ return {
       null_ls.builtins.diagnostics.eslint_d,
       null_ls.builtins.formatting.eslint_d,
 
+      null_ls.builtins.formatting.jq,
+
       -- latex
       null_ls.builtins.formatting.bibclean,
       null_ls.builtins.formatting.latexindent.with({
@@ -45,15 +58,35 @@ return {
 
       -- markdown
       null_ls.builtins.diagnostics.alex,
+      null_ls.builtins.diagnostics.markdownlint,
+
+      -- ocaml
+      null_ls.builtins.formatting.ocamlformat,
 
       -- python
       null_ls.builtins.diagnostics.flake8,
+      null_ls.builtins.diagnostics.mypy,
+      null_ls.builtins.diagnostics.pylint,
       null_ls.builtins.formatting.autopep8,
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.isort,
+
+      -- ruby
+      null_ls.builtins.diagnostics.rubocop,
+
+      -- rust
+      null_ls.builtins.formatting.rustfmt,
 
       -- shell
       null_ls.builtins.code_actions.shellcheck,
-      null_ls.builtins.formatting.shfmt,
       null_ls.builtins.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+      null_ls.builtins.formatting.beautysh,
+      null_ls.builtins.formatting.shfmt,
+
+      -- yaml
+      null_ls.builtins.diagnostics.yamllint,
+      null_ls.builtins.formatting.yamlfmt,
+
     }
     null_ls.setup({
       sources = sources,

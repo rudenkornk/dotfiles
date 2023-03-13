1636c7715f560c5c3fb5105dfe322882e51652b9
Nikita Rudenko <rudenkornk@gmail.com>
2023-03-13 17:51:29 +0300

    [terminals] Add wezterm terminal
    Actually it is laggy on my machine

---
+++ roles/terminals/files/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+local config = {
+  font = wezterm.font("FiraCode Nerd Font Mono"),
+  color_scheme = "Dracula",
+  hide_tab_bar_if_only_one_tab = true,
+  font_size = 11.0,
+}
+
+return config

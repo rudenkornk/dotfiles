60c3ec2e4a880dfa922e356db6f9549702818adf
Nikita Rudenko <rudenkornk@gmail.com>
2023-03-14 18:25:11 +0300

    [neovim] Add simple configuration version management

---
+++ scripts/select_nvim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+version=${1:-1}
+
+mkdir --parents ~/.local/share/nvim_versions/nvim_"$version"/cache
+mkdir --parents ~/.local/share/nvim_versions/nvim_"$version"/config
+mkdir --parents ~/.local/share/nvim_versions/nvim_"$version"/share
+
+ln --symbolic --force --no-target-directory ~/.local/share/nvim_versions/nvim_"$version"/cache ~/.cache/nvim
+ln --symbolic --force --no-target-directory ~/.local/share/nvim_versions/nvim_"$version"/config ~/.config/nvim
+ln --symbolic --force --no-target-directory ~/.local/share/nvim_versions/nvim_"$version"/share ~/.local/share/nvim

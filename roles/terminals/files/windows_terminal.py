e12bd70c64e6a18449b296862faa3e054ab240bf
Nikita Rudenko <rudenkornk@gmail.com>
2023-04-13 03:59:38 +0300

    update(terminals): Update windows terminal setup script

--- roles/terminals/files/windows_terminal.py
+++ roles/terminals/files/windows_terminal.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
-import json
-import os
-import subprocess
+import difflib as _difflib
+import json as _json
+import logging as _logging
+import os as _os
+import shutil as _shutil
+import subprocess as _subprocess
+import tempfile as _tempfile
 from pathlib import Path as _Path
 from typing import Any as _Any
 
-from lupa import LuaRuntime  # type: ignore
+from lupa import LuaRuntime as _LuaRuntime  # type: ignore
 
-lua = LuaRuntime(unpack_returned_tuples=True)
+lua = _LuaRuntime(unpack_returned_tuples=True)
+
+_logger = _logging.getLogger(__name__)
 
 
-def get_lua_table(theme_path: _Path) -> _Any:
+def _get_lua_table(theme_path: _Path) -> _Any:
     func_begin = """\
     function()
     vim = { opt = { bg = {} } }
@@ -27,7 +33,7 @@ def get_lua_table(theme_path: _Path) -> _Any:
     return lua_table
 
 
-def get_win_theme(theme_path: _Path) -> dict[str, str]:
+def _get_win_theme(theme_path: _Path) -> dict[str, str]:
     windows_terminal_map = {
         "background": "black",
         "black": "black",
@@ -50,7 +56,7 @@ def get_win_theme(theme_path: _Path) -> dict[str, str]:
         "white": "light_grey",
         "yellow": "yellow",
     }
-    lua_table = get_lua_table(theme_path)
+    lua_table = _get_lua_table(theme_path)
     win_theme = {}
     for win, nvchad in windows_terminal_map.items():
         win_theme[win] = lua_table.base_30[nvchad].upper()
@@ -63,27 +69,104 @@ def get_win_theme(theme_path: _Path) -> dict[str, str]:
     return win_theme
 
 
-def get_win_themes() -> list[dict[str, str]]:
-    themes_path = _Path.home() / ".local/share/nvim/site/pack/packer/start/base46/lua/base46/themes/"
-    win_themes = []
-    for theme in themes_path.glob("*.lua"):
-        win_theme = get_win_theme(theme)
-        win_themes.append(win_theme)
-    win_themes = sorted(win_themes, key=lambda x: x["name"])
+def _get_win_themes() -> list[dict[str, str]]:
+    themes_path = _Path.home() / ".local/share/nvim/lazy/base46/lua/base46/themes/"
+    cwd = _Path.cwd()
+    try:
+        _os.chdir(_Path(__file__).parent)
+        win_themes = []
+        for theme in themes_path.glob("*.lua"):
+            win_theme = _get_win_theme(theme)
+            win_themes.append(win_theme)
+        win_themes = sorted(win_themes, key=lambda x: x["name"])
+    finally:
+        _os.chdir(cwd)
     return win_themes
 
 
-# Not implemented
-def write_win_themes(win_themes: list[dict[str, str]]) -> None:
-    win_profile = subprocess.run(["wslvar", "USERPROFILE"], check=True, capture_output=True, text=True).stdout.strip()
+def _get_settings() -> dict[str, _Any]:
+    return {
+        "actions": [
+            {"command": "unbound", "keys": "ctrl+c"},
+            {"command": "unbound", "keys": "ctrl+v"},
+            {"command": {"action": "splitPane", "split": "auto", "splitMode": "duplicate"}, "keys": "alt+shift+d"},
+            {"command": {"action": "copy", "singleLine": False}, "keys": "ctrl+alt+c"},
+            {"command": "paste", "keys": "ctrl+alt+v"},
+            {"command": "find", "keys": "ctrl+shift+f"},
+            {"command": {"action": "openSettings", "target": "settingsUI"}, "keys": "ctrl+alt+s"},
+            {"command": "toggleFocusMode", "keys": "ctrl+alt+f"},
+            {"command": {"action": "closeTab"}, "keys": "ctrl+alt+w"},
+        ],
+        "copyOnSelect": False,
+        "launchMode": "maximizedFocus",
+        "profiles": {
+            "defaults": {
+                "altGrAliasing": False,
+                "font": {"face": "FiraCode Nerd Font Mono Retina", "size": 12},
+                "historySize": 100000,
+                "opacity": 99,
+                "padding": "0",
+                "useAcrylic": True,
+                "useAtlasEngine": True,
+            }
+        },
+        "schemes": _get_win_themes(),
+    }
+
+
+def _update_list(lst: list[dict[str, _Any]], vals: list[dict[str, _Any]], id: str) -> None:
+    keys = {key[id]: i for i, key in enumerate(lst)}
+    for val in vals:
+        if val[id] in keys:
+            lst[keys[val[id]]] = val
+        else:
+            lst.append(val)
+
+
+def update_settings(settings: dict[str, _Any]) -> None:
+    _logger.info("Collecting new settings...")
+    new_settings = _get_settings()
+    _logger.info(f"Custom actions: {len(new_settings['actions'])}")
+    _logger.info(f"Custom schemes: {len(new_settings['schemes'])}")
+    _update_list(settings["actions"], new_settings["actions"], "keys")
+    _update_list(settings["schemes"], new_settings["schemes"], "name")
+    settings["profiles"]["defaults"].update(new_settings["profiles"]["defaults"])
+    settings["launchMode"] = new_settings["launchMode"]
+    settings["copyOnSelect"] = new_settings["copyOnSelect"]
+
+
+def _backup_path(path: _Path) -> _Path:
+    i = 0
+    while True:
+        backup_path = path.with_suffix(f".bak_{i}")
+        if not backup_path.exists():
+            return backup_path
+        i += 1
+
+
+def update_config() -> None:
+    _logger.info("Reading WSL vars...")
+    win_profile = _subprocess.run(["wslvar", "USERPROFILE"], check=True, capture_output=True, text=True).stdout.strip()
     win_home = _Path(
-        subprocess.run(["wslpath", win_profile], check=True, capture_output=True, text=True).stdout.strip()
+        _subprocess.run(["wslpath", win_profile], check=True, capture_output=True, text=True).stdout.strip()
     )
     settings_path = win_home / "AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
-    settings = json.loads(settings_path.read_text())
+    settings = _json.loads(settings_path.read_text())
+    original = _json.dumps(settings, indent=2)
+    update_settings(settings)
+    updated = _json.dumps(settings, indent=2)
+    diff = _difflib.unified_diff(original.splitlines(), updated.splitlines(), lineterm="")
+    _logger.debug("Settings difference:")
+    _logger.debug("\n".join(diff))
+
+    backup_path = _backup_path(settings_path)
+    _logger.info(f"Backing up old settings to {backup_path}...")
+    _shutil.copy(settings_path, backup_path)
+    _logger.info("Writing new settings...")
+    with open(settings_path, "w", encoding="UTF-8") as json_file:
+        json_file.write(updated)
 
 
 if __name__ == "__main__":
-    os.chdir(_Path(__file__).parent)
-    win_themes = get_win_themes()
-    print(json.dumps(win_themes, indent=4))
+    _logging.basicConfig(format="%(message)s", level=_logging.DEBUG)
+    update_config()

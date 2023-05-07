d13608207c3d237f3f8122fab4e3a5e687d51581
Nikita Rudenko <rudenkornk@gmail.com>
2023-05-07 16:25:04 +0300

    update(scripts): update

--- support.py
+++ support.py
@@ -9,14 +9,10 @@ import scripts.utils as _utils
 _logger = _logging.getLogger("scripts")
 
 
+@_utils.main(_logger)
+def _main() -> None:
+    _setup.process_shell_args(_sys.argv)
+
+
 if __name__ == "__main__":
-    _utils.format_logging()
-    _logger.setLevel(_logging.INFO)
-    try:
-        _setup.process_shell_args(_sys.argv)
-    except Exception as e:  # pylint: disable=broad-exception-caught
-        _logger.error(f"{type(e).__name__}: {e}")
-        _sys.exit(1)
-    except KeyboardInterrupt:
-        _logger.error("==== Keyboard Interrupt ====")
-        _sys.exit(1)
+    _main()

60e15f2a7c9660592771a4d2ac43d6ca5c914106
Nikita Rudenko <rudenkornk@gmail.com>
2023-09-01 17:25:58 +0300

    refactor(shell): auto-activate python venv

--- roles/shell/files/shell.fish
+++ roles/shell/files/shell.fish
@@ -2,3 +2,17 @@ fish_add_path "$HOME/.local/bin"
 
 # Prevent accidentally closing terminal
 bind \cd delete-char
+
+set current "$(pwd)"
+set host $(hostname)
+
+while string match --quiet "$HOME*" "$current"
+  set activate "$current/.makepy/artifacts/venv/$(hostname)/bin/activate.fish"
+  if test -f $activate
+    source $activate
+    return
+  end
+  set current (dirname "$current")
+end
+
+source $HOME/.venv/bin/activate.fish

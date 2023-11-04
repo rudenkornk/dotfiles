076b6a8440be6cf9b996cd34a133989c2741e0cd
Nikita Rudenko <rudenkornk@gmail.com>
2023-11-04 19:49:29 +0300

    fix: fix wrong cursor

--- roles/shell/files/shell.fish
+++ roles/shell/files/shell.fish
@@ -3,6 +3,9 @@ fish_add_path "$HOME/.local/bin"
 # Prevent accidentally closing terminal
 bind \cd delete-char
 
+# Set vertical line cursor
+printf '\033[6 q'
+
 set current "$(pwd)"
 set host $(hostname)
 

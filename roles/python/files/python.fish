0c10c997e4524be073101d913baef17dd9d2e611
Nikita Rudenko <rudenkornk@gmail.com>
2023-11-11 20:01:18 +0300

    refactor(python): remove venv auto activation
    Use https://github.com/linux-cultist/venv-selector.nvim instead

--- roles/python/files/python.fish
+++ roles/python/files/python.fish
@@ -1,13 +1,2 @@
 set current "$(pwd)"
 set host $(hostname)
-
-while string match --quiet "$HOME*" "$current"
-  set activate "$current/.makepy/artifacts/venv/$(hostname)/bin/activate.fish"
-  if test -f $activate
-    source $activate
-    return
-  end
-  set current (dirname "$current")
-end
-
-source $HOME/.venv/bin/activate.fish

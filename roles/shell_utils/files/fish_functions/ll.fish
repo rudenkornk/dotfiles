5efc2842ed84e6cc101c8c75f13ee4b45cdcea79
Nikita Rudenko <rudenkornk@gmail.com>
2024-04-27 20:47:52 +0300

    feat(shell_utils): migrate from exa to eza

--- roles/shell_utils/files/fish_functions/ll.fish
+++ roles/shell_utils/files/fish_functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps exa
-  exa --all --long --classify --icons --octal-permissions $argv
+function ll --wraps eza
+  eza --all --long --classify --icons --octal-permissions $argv
 end

d1603da34c062e0bb5716625d1612e89ca85203d
Nikita Rudenko <rudenkornk@gmail.com>
2024-04-30 17:22:32 +0300

    refactor(neovim): add more terminal mappings

--- roles/neovim/files/lazy/lua/config/keymaps.lua
+++ roles/neovim/files/lazy/lua/config/keymaps.lua
@@ -12,6 +12,12 @@ vim.keymap.set({ "n", "x" }, "<C-e>", "4<C-e>", { silent = true, desc = "Scroll 
 vim.keymap.set({ "n", "x" }, "<C-y>", "4<C-y>", { silent = true, desc = "Scroll down" })
 vim.keymap.set({ "x" }, "<leader>vs", ":sort i<CR>", { desc = "Sort selected lines" })
 vim.keymap.set({ "n" }, "<C-q>", "a<C-^><ESC>", { desc = "Toggle keyboard layout" })
+
+vim.keymap.set({ "n" }, "<A-m>", function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set({ "t" }, "<A-m>", "<cmd>close<cr>", { desc = "Close terminal" })
+
 vim.keymap.set({ "i" }, "<C-q>", "<C-^>", { desc = "Toggle keyboard layout" })
 
 -- This should be inside options.lua, but for some reason it doesn't work there

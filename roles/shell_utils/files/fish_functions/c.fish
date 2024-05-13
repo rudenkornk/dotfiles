732713b09309eba892b98ae3f5c7f9220f446df0
Nikita Rudenko <nikita.rudenko@syntacore.com>
2024-05-13 18:30:04 +0300

    refactor(shell_utils): use z instead of its unclear alias

--- roles/shell_utils/files/fish_functions/c.fish
+++ roles/shell_utils/files/fish_functions/c.fish
@@ -1,9 +1,9 @@
-function c --wraps __zoxide_z
+function c --wraps z
   if test "$argv" = "-"
      or test "$argv" = ""
     popd && eza --classify --icons
   else
     pushd .
-    __zoxide_z $argv && eza --classify --icons
+    z $argv && eza --classify --icons
   end
 end

c42e9563462a558946c84078ece4bea3b1117b1b
Nikita Rudenko <rudenkornk@gmail.com>
2024-05-18 17:59:05 +0300

    refactor(scripts): format bash scripts

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -2,7 +2,7 @@
 
 # Credits https://askubuntu.com/a/803316
 colors=256
-for (( n=0; n < colors; n++ )) do
-    printf " [%d] $(tput setaf $n)%s$(tput sgr0)" $n "■■■■■■■■■■
+for ((n = 0; n < colors; n++)); do
+  printf " [%d] $(tput setaf $n)%s$(tput sgr0)" $n "■■■■■■■■■■
 "
 done

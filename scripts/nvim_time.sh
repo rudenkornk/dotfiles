c7fbd017514eabbd7cc9af88d60d725f61a8b71f
Nikita Rudenko <rudenkornk@gmail.com>
2024-05-18 17:59:05 +0300

    refactor(scripts): format bash scripts

--- scripts/nvim_time.sh
+++ scripts/nvim_time.sh
@@ -5,11 +5,10 @@ F=${1:-$HOME/projects/dotfiles/roles/neovim/files/nvchad2/plugins/init.lua}
 N=30
 SUM=0
 
-for ((i=1;i<=N;i++)); do
+for ((i = 1; i <= N; i++)); do
   nvim --startuptime time --headless "$F" -c q
   T=$(tail -n 1 time | awk '{print $1}')
   SUM=$(echo "$SUM + $T" | bc)
   rm --force time
 done
 echo "Average: $(echo "$SUM / $N" | bc) ms"
-

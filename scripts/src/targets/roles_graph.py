f1144d0307392efc17b5246d01f7822e5073e756
Nikita Rudenko <rudenkornk@gmail.com>
2024-06-12 17:37:22 +0300

    refactor(lint): add secrets dependencies check

--- scripts/src/targets/roles_graph.py
+++ scripts/src/targets/roles_graph.py
@@ -4,17 +4,14 @@ from .. import utils as _utils
 from .bootstrap import bootstrap as _bootstrap
 
 
-def generate_png(view: bool = False) -> None:
-    _bootstrap()
-
-    graph = _graphviz.Digraph(name="roles")
-
+def roles_graph() -> dict[str, set[str]]:
     roles_path = _utils.REPO_PATH / "roles"
+    graph: dict[str, set[str]] = {}
     for role_path in roles_path.iterdir():
         if not role_path.is_dir():
             continue
         role = role_path.stem
-        graph.node(role)
+        graph[role] = set()
 
         meta_path = role_path / "meta"
         if (meta_path / "main.yml").exists():
@@ -27,7 +24,22 @@ def generate_png(view: bool = False) -> None:
         yaml, _ = _utils.yaml_read(dependencies_path)
         assert isinstance(yaml, dict)
         for dep in yaml["dependencies"]:
-            graph.edge(role, dep["role"])
+            graph[role].add(dep["role"])
+
+    return graph
+
+
+def generate_png(view: bool = False) -> None:
+    _bootstrap()
+
+    graph = _graphviz.Digraph(name="roles")
+
+    str_graph = roles_graph()
+
+    for node, dependencies in str_graph.items():
+        graph.node(node)
+        for dep in dependencies:
+            graph.edge(node, dep)
 
     output_path = _utils.ARTIFACTS_PATH / "roles_graph"
     graph.format = "png"

5dbc4a8cea6c43e6cb73421255db1c767113a71f
Nikita Rudenko <rudenkornk@gmail.com>
2024-07-05 21:12:20 +0300

    refactor(nvim): run nvim with proxy

--- roles/neovim/files/bash.sh
+++ roles/neovim/files/bash.sh
@@ -6,5 +6,3 @@ export EDITOR="$VISUAL"
 alias v=nvim
 alias vi=nvim
 alias vd="nvim -d"
-
-export PATH="$HOME/.local/nvim-linux64/bin:$PATH"
--- roles/neovim/files/neovim.fish
+++ roles/neovim/files/neovim.fish
@@ -1,4 +1,2 @@
-fish_add_path $HOME/.local/nvim-linux64/bin
-
 set -Ux VISUAL nvim
 set -Ux EDITOR nvim

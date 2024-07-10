d24d92e7a8c0689d3430f121ec56f06876e4241d
Nikita Rudenko <rudenkornk@gmail.com>
2024-07-10 22:25:59 +0300

    refactor(shell_utils): refactor git shortcuts

--- roles/shell_utils/files/shell_utils.fish
+++ roles/shell_utils/files/shell_utils.fish
@@ -1,5 +1,33 @@
 fish_add_path ~/.fzf/bin
 
+# Control keys:
+# C-a beginning of the line
+# C-b one char backwards
+# C-c interrupt process
+# C-d delete one char
+# C-e end of the line
+# C-f one char forward
+# C-g fzf git log
+# C-h goto left window
+# C-i show suggestions
+# C-j goto lower window
+# C-k goto upper window
+# C-l goto right window
+# C-m enter
+# C-n next history command
+# C-o fzf git status
+# C-p previous history command
+# C-q fzf search files with ripgrep
+# C-r fzf command history
+# C-s tmux reserved
+# C-t fzf search files
+# C-u delete all to the left
+# C-v fzf search variables
+# C-w delete one word to the left
+# C-x fzf search processes (previously copy fish command)
+# C-y fzf yazi
+# C-z undo
+
 # fzf has three ends: how it inputs stuff, how it displays it and how it displays stuff's content.
 # FZF_DEFAULT_COMMAND and FZF_CTRL_T_COMMAND is responsible for input, we use fd finder.
 # FZF_DEFAULT_OPTS is responsible for displaying stuff and managing it.
@@ -9,16 +37,15 @@ fish_add_path ~/.fzf/bin
 
 
 set --export FZF_DEFAULT_OPTS \
-  --ansi \
-  --bind ctrl-b:half-page-up,ctrl-f:half-page-down,ctrl-u:preview-page-up,ctrl-d:preview-page-down \
-  --border \
-  --cycle \
-  --height=90% \
-  --layout=reverse \
-  --marker="*" \
-  --multi \
-  --preview-window=wrap \
-
+    --ansi \
+    --bind ctrl-b:half-page-up,ctrl-f:half-page-down,ctrl-u:preview-page-up,ctrl-d:preview-page-down \
+    --border \
+    --cycle \
+    --height=90% \
+    --layout=reverse \
+    --marker="*" \
+    --multi \
+    --preview-window=wrap
 
 # Do not set here "--color": it will interfere with fzf.fish
 set --export fzf_fd_opts \
@@ -37,8 +64,7 @@ set --export fzf_fd_opts \
     --exclude site-packages \
     --follow \
     --no-ignore \
-    --hidden \
-
+    --hidden
 set --export fzf_ps_opts \
     -A \
     -o 'pid user %cpu %mem time command' \
@@ -47,10 +73,16 @@ set --export fzf_ps_opts \
 set --export fzf_processes_opts \
     --bind='ctrl-x:reload(ps $fzf_ps_opts)' \
     --header-lines=1 \
-    --header='Press CTRL-X to reload\n\n' \
+    --header='Press CTRL-X to reload\n\n'
 
+fzf_configure_bindings --directory=\ct --git_log=\co --git_status= --history=\cr --processes=\cx --variables=\cv --files=\cq
 
-set --export MANROFFOPT "-c"
+for mode in default insert
+    bind --mode $mode \cg __fish_disable_focus lazygit
+    bind --mode $mode \cy __fish_disable_focus yazi
+end
+
+set --export MANROFFOPT -c
 set --export MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 zoxide init fish | source

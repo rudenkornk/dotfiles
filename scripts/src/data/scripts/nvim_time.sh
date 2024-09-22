6ffc8709e3c48500a9cdf47c17323699f2e600a0
Nikita Rudenko <rudenkornk@gmail.com>
2024-09-22 12:54:24 +0300

    refactor(scripts): fix broken nvim_time.sh script

--- scripts/src/data/scripts/nvim_time.sh
+++ scripts/src/data/scripts/nvim_time.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-F=${1:-$HOME/projects/dotfiles/roles/neovim/files/nvchad2/plugins/init.lua}
+F=${1:-$HOME/projects/dotfiles/uv.lock}
 
-N=30
+N=15
 SUM=0
 
 for ((i = 1; i <= N; i++)); do
   nvim --startuptime time --headless "$F" -c q
-  T=$(tail -n 1 time | awk '{print $1}')
+  T=$(tail -n 2 time | head -1 | awk '{print $1}')
   SUM=$(echo "$SUM + $T" | bc)
   rm --force time
 done

36d1d4d096a792b947333e0caad9aa6f3aa2f834
Nikita Rudenko <rudenkornk@gmail.com>
2025-04-26 00:40:36 +0300

    refactor(neovim): fix snacks.nvim navigation

--- roles/neovim/files/lazy/lua/plugins/configs/snacks.lua
+++ roles/neovim/files/lazy/lua/plugins/configs/snacks.lua
@@ -11,6 +11,29 @@ M.opts = {
       list_scroll_up_4 = function(picker)
         picker.list:scroll(-4, false)
       end,
+      win_right = function(picker)
+        vim.api.nvim_command("wincmd l")
+      end,
+    },
+    sources = {
+      -- https://github.com/folke/snacks.nvim/discussions/1802
+      -- https://github.com/aserowy/tmux.nvim/issues/133
+      -- https://github.com/folke/snacks.nvim/issues/1350
+      -- https://github.com/folke/snacks.nvim/discussions/1332
+      explorer = {
+        win = {
+          input = {
+            keys = {
+              ["<C-l>"] = { "win_right", mode = { "i", "n" } }, -- W/A for tmux navigation
+            },
+          },
+          list = {
+            keys = {
+              ["<C-l>"] = { "win_right", mode = { "i", "n" } }, -- W/A for tmux navigation
+            },
+          },
+        },
+      },
     },
     win = {
       input = {

532b6cc12faf59b978b2c78a6c6005097b31d4b0
Nikita Rudenko <rudenkornk@gmail.com>
2025-04-27 21:30:21 +0300

    refactor(neovim): unset PROXY vars in neovim terminal

--- roles/neovim/files/bash.sh
+++ roles/neovim/files/bash.sh
@@ -6,3 +6,9 @@ export EDITOR="$VISUAL"
 alias v=nvim
 alias vi=nvim
 alias vd="nvim -d"
+
+# See neovim.fish for explanation.
+if [[ -n "$NVIM" && -n "$MYVIMRC" ]]; then
+  unset HTTP_PROXY
+  unset HTTPS_PROXY
+fi
--- roles/neovim/files/neovim.fish
+++ roles/neovim/files/neovim.fish
@@ -1,2 +1,15 @@
 set -Ux VISUAL nvim
 set -Ux EDITOR nvim
+
+# This a tricky one.
+# Inside a nvim started we set HTTP(S)_PROXY for Copilot to work.
+# This is done specifically for neovim, so not to touch any other tools.
+# However, if terminal is started from inside neovim, PROXY vars are set,
+# and this might break other tools, especially in corporate environment.
+# Thus, we need to unset this variables.
+# Since HTTP(S)_PROXY are common vars, we would like to unset them only if
+# they were specifically set for neovim.
+if test -n "$NVIM" && test -n "$MYVIMRC"
+    set --unexport HTTP_PROXY
+    set --unexport HTTPS_PROXY
+end

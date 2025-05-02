0face62ef697cfaeb4abe9d917b63079dedc0b7b
Nikita Rudenko <rudenkornk@gmail.com>
2025-05-02 03:59:29 +0300

    refactor(scripts): migrate to rich logging

--- __main__.py
+++ __main__.py
@@ -6,8 +6,8 @@ from scripts import utils
 from scripts.cli import cli
 
 
-@utils.main()
 def _main() -> None:
+    utils.setup_logger()
     cli(max_content_width=shutil.get_terminal_size(fallback=(120, 24)).columns)
 
 

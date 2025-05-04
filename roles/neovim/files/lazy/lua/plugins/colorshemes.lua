713dedc627cfadcdc0b0897ce882573f18768b0a
Nikita Rudenko <rudenkornk@gmail.com>
2025-05-05 01:42:50 +0300

    refactor(neovim): set tokyonight night theme

--- roles/neovim/files/lazy/lua/plugins/colorshemes.lua
+++ roles/neovim/files/lazy/lua/plugins/colorshemes.lua
@@ -3,7 +3,7 @@ return {
 
   { "catppuccin/nvim" },
   { "ellisonleao/gruvbox.nvim" },
-  { "folke/tokyonight.nvim", opts = { style = "moon" } }, -- moon, storm, night, day
+  { "folke/tokyonight.nvim", opts = { style = "night" } }, -- moon, storm, night, day
   { "maxmx03/FluoroMachine.nvim", opts = { glow = true, theme = "fluoromachine" } }, -- fluoromachine, retrowave, delta
   { "Mofiqul/dracula.nvim" },
   { "Mofiqul/vscode.nvim" }, -- picks vim.o.background settings for light or dark
@@ -11,4 +11,5 @@ return {
   { "rose-pine/neovim", name = "rose-pine", opts = { variant = "auto" } }, -- auto, main, moon, dawn
   { "sainnhe/everforest" },
   { "shaunsingh/nord.nvim" },
+  { "rebelot/kanagawa.nvim" }, -- broken visuals :(
 }

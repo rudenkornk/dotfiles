532bf578c8d5c57e9b9995f4dd00464349462365
Nikita Rudenko <rudenkornk@gmail.com>
2025-05-25 02:39:32 +0300

    refactor(neovim): amend AI scores

--- roles/neovim/files/lazy/lua/plugins/configs/blink.lua
+++ roles/neovim/files/lazy/lua/plugins/configs/blink.lua
@@ -14,10 +14,10 @@ M.opts = {
         opts = { label = " ", all_panes = true },
       },
       supermaven = {
-        score_offset = 1,
+        score_offset = 10,
       },
       copilot = {
-        score_offset = 1,
+        score_offset = 10,
       },
     },
   },

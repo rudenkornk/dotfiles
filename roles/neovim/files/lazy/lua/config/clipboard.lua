27f11bb810c09dc2ba3c42a38837d03d4fd32736
Nikita Rudenko <rudenkornk@gmail.com>
2025-05-24 15:03:01 +0300

    refactor(neovim): amend clipboard setup

--- roles/neovim/files/lazy/lua/config/clipboard.lua
+++ roles/neovim/files/lazy/lua/config/clipboard.lua
@@ -1,3 +1,9 @@
+-- https://github.com/LazyVim/LazyVim/blob/25abbf546d564dc484cf903804661ba12de45507/lua/lazyvim/config/options.lua#L57
+
+if vim.env.SSH_TTY then
+  return ""
+end
+
 -- see https://www.reddit.com/r/neovim/comments/ab01n8/improve_neovim_startup_by_60ms_for_free_on_macos/
 -- and https://www.reddit.com/r/neovim/comments/wb2obw/how_to_setup_gclipboard_using_lua/
 local clipboards = {
@@ -27,4 +33,4 @@ local clipboards = {
   },
 }
 
-return clipboards["{{ clipboard }}"]
+return clipboards[require("config.this_clipboard")]

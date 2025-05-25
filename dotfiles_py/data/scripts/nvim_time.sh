e16bb82894279125a6e014574d945f3cf5272ac4
Nikita Rudenko <rudenkornk@gmail.com>
2025-05-26 00:07:24 +0300

    refactor(shell_utils): add hyperfine

--- dotfiles_py/data/scripts/nvim_time.sh
+++ dotfiles_py/data/scripts/nvim_time.sh
@@ -2,13 +2,4 @@
 
 F=${1:-$HOME/projects/dotfiles/uv.lock}
 
-N=15
-SUM=0
-
-for ((i = 1; i <= N; i++)); do
-  nvim --startuptime time --headless "$F" -c q
-  T=$(tail -n 2 time | head -1 | awk '{print $1}')
-  SUM=$(echo "$SUM + $T" | bc)
-  rm --force time
-done
-echo "Average: $(echo "$SUM / $N" | bc) ms"
+hyperfine "nvim $F -c q"

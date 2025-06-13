aa1d3d706d6eeb82b98b0667f1e0eae082109668
Nikita Rudenko <rudenkornk@gmail.com>
2025-06-13 20:18:48 +0300

    refactor(scripts): replace click with typer

--- dotfiles_py/cli.py
+++ dotfiles_py/cli.py
@@ -1,12 +1,13 @@
 import getpass
 import logging
 import secrets
-import shutil
 import string
+from enum import StrEnum, auto
 from pathlib import Path
+from typing import Annotated
 
 import click
-from click_help_colors import HelpColorsGroup
+import typer
 
 from . import utils
 from .targets import bootstrap, oh_my_posh
@@ -20,98 +21,141 @@ from .targets import update as update_target
 _logger = logging.getLogger(__name__)
 
 
-@click.group(
-    help="main script for spawning Ansible operations or managing dotfiles.",
-    context_settings={"help_option_names": ["-h", "--help"], "show_default": True},
-    cls=HelpColorsGroup,
-    help_headers_color="yellow",
-    help_options_color="green",
+app = typer.Typer(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    help="Main script for spawning Ansible operations or managing dotfiles.",
 )
-@click.option(
-    "-l",
-    "--log-level",
-    type=click.Choice(["debug", "info", "warning", "error", "critical", "d", "i", "w", "e", "c"], case_sensitive=False),
-    default="info",
-    help="Logging level.",
-)
-def cli(log_level: str) -> None:
-    level_map = {
-        "debug": logging.DEBUG,
-        "info": logging.INFO,
-        "warning": logging.WARNING,
-        "error": logging.ERROR,
-        "d": logging.DEBUG,
-        "i": logging.INFO,
-        "w": logging.WARNING,
-        "e": logging.ERROR,
-        "c": logging.CRITICAL,
-    }
-    logging.getLogger().setLevel(level_map[log_level])
 
 
-@cli.command(help="Configure systems.")
-@click.option(
-    "-t",
-    "--host",
-    type=click.Choice(list(config_target.hosts().keys())),
-    default=["localhost"],
-    help="Hosts to configure. Note, that 'dotfiles_*' hostnames have a special meaning: "
-    "they are actually a podman containers, which are automatically started and used for testing purposes.",
-    multiple=True,
-)
-@click.option(
-    "-u",
-    "--user",
-    type=str,
-    default=getpass.getuser(),
-    help="Target user to configure.",
-)
-@click.option(
-    "-v",
-    "--verify-unchanged",
-    is_flag=True,
-    help="This is an idempotency check. If anything was changed, fail.",
-)
-@click.option(
-    "-m",
-    "--mode",
-    type=click.Choice(config_target.ConfigMode.values()),
-    default="full",
-    help="Configuration mode. "
-    "'bootstrap' will only install python and create target user on the host. "
-    "'minimal' will install only the most critical parts like credentials and shell utils. "
-    "'server' will skip all GUI tools. "
-    "'full' will do full configuration.",
-)
-def config(*, host: list[str], user: str, verify_unchanged: bool, mode: str) -> None:
+class LoggingLevelOpt(StrEnum):
+    s = auto()
+    spam = auto()
+    d = auto()
+    debug = auto()
+    v = auto()
+    verbose = auto()
+    i = auto()
+    info = auto()
+    n = auto()
+    notice = auto()
+    w = auto()
+    warning = auto()
+    u = auto()
+    success = auto()
+    e = auto()
+    error = auto()
+    c = auto()
+    critical = auto()
+
+    @property
+    def to_logging_level(self) -> int:
+        return {
+            "s": logging.DEBUG - 5,
+            "spam": logging.DEBUG - 5,
+            "d": logging.DEBUG,
+            "debug": logging.DEBUG,
+            "v": logging.INFO - 5,
+            "verbose": logging.INFO - 5,
+            "i": logging.INFO,
+            "info": logging.INFO,
+            "n": logging.WARNING - 5,
+            "notice": logging.WARNING - 5,
+            "w": logging.WARNING,
+            "warning": logging.WARNING,
+            "u": logging.ERROR - 5,
+            "success": logging.ERROR - 5,
+            "e": logging.ERROR,
+            "error": logging.ERROR,
+            "c": logging.CRITICAL,
+            "critical": logging.CRITICAL,
+        }[self.value]
+
+
+@app.callback()
+def setup_app(
+    *,
+    log_level: Annotated[
+        LoggingLevelOpt, typer.Option("-l", "--log-level", help="Logging level.", case_sensitive=False)
+    ] = LoggingLevelOpt.info,
+) -> None:
+    logging.getLogger().setLevel(log_level.to_logging_level)
+    utils.setup_logger()
+
+
+@app.command()
+def config(
+    *,
+    host: Annotated[
+        list[str],
+        typer.Option(
+            "-t",
+            "--host",
+            click_type=click.Choice(list(config_target.hosts().keys())),
+            help="Hosts to configure. Note, that 'dotfiles_*' hostnames have a special meaning: "
+            "they are actually podman containers, which are automatically started and used for testing purposes.",
+            show_choices=True,
+            case_sensitive=False,
+        ),
+    ] = ["localhost"],  # noqa: B006
+    user: Annotated[
+        str,
+        typer.Option("-u", "--user", help="Target user to configure."),
+    ] = getpass.getuser(),
+    verify_unchanged: Annotated[
+        bool,
+        typer.Option("-v", "--verify-unchanged", help="This is an idempotency check. If anything was changed, fail."),
+    ] = False,
+    mode: Annotated[
+        str,
+        typer.Option(
+            "-m",
+            "--mode",
+            click_type=click.Choice(config_target.ConfigMode.values()),
+            help="Configuration mode. "
+            "'bootstrap' will only install python and create target user on the host. "
+            "'minimal' will install only the most critical parts like credentials and shell utils. "
+            "'server' will skip all GUI tools. "
+            "'full' will do full configuration.",
+        ),
+    ] = "full",
+) -> None:
+    """Configure systems."""
     mode_enum = config_target.ConfigMode[mode.upper()]
     config_target.config(hostnames=host, user=user, verify_unchanged=verify_unchanged, mode=mode_enum)
 
 
-@cli.command(help="Check that main.sh bootstrapping script works correctly on different systems.")
-@click.option(
-    "-i",
-    "--image",
-    type=click.Choice(config_target.images()),
-    default=[config_target.images()[0]],
-    help="Target container image, where to check bootstrap.sh script.",
-    multiple=True,
-)
-def check_bootstrap(image: list[str]) -> None:
+@app.command()
+def check_bootstrap(
+    *,
+    image: Annotated[
+        list[str],
+        typer.Option(
+            "-i",
+            "--image",
+            help="Target container image, where to check bootstrap.sh script.",
+            click_type=click.Choice(config_target.images()),
+        ),
+    ] = [config_target.images()[0]],  # noqa: B006, B008
+) -> None:
+    """Check that main.sh bootstrapping script works correctly on different systems."""
     for image_ in image:
         bootstrap.check_bootstrap(image_)
 
 
-@cli.command(help="Lint code.")
-@click.option(
-    "-o",
-    "--only",
-    type=click.Choice(["all", *lint_target.lint_choices()]),
-    default=["all"],
-    help="Only lint these.",
-    multiple=True,
-)
-def lint(*, only: list[str]) -> None:
+@app.command()
+def lint(
+    *,
+    only: Annotated[
+        list[str],
+        typer.Option(
+            "-o",
+            "--only",
+            help="Only lint these.",
+            click_type=click.Choice(["all", *lint_target.lint_choices()]),
+        ),
+    ] = ["all"],  # noqa: B006
+) -> None:
+    """Lint code."""
     if "all" in only:
         args = dict.fromkeys(lint_target.lint_choices(), True)
     else:
@@ -119,76 +163,84 @@ def lint(*, only: list[str]) -> None:
     lint_target.lint_code(**args)
 
 
-@cli.command(name="format", help="Format code.")
+@app.command(name="format")
 def format_code() -> None:
+    """Format code."""
     lint_target.format_code()
 
 
-@cli.command(help="Setup repo git hooks.")
+@app.command()
 def hooks() -> None:
+    """Perform git hooks setup in repo."""
     hooks_target.hooks()
 
 
-@cli.command(help="Update components versions.")
-@click.option(
-    "-c",
-    "--components",
-    type=click.Choice(["all", *update_target.get_update_choices()]),
-    default=["all"],
-    help="Component to update.",
-    multiple=True,
-)
-@click.option("-d", "--dry-run", is_flag=True, help="Do not actually change versions.")
-def update(*, components: list[str], dry_run: bool) -> None:
+@app.command()
+def update(
+    *,
+    components: Annotated[
+        list[str],
+        typer.Option(
+            "-c",
+            "--components",
+            help="Component to update.",
+            click_type=click.Choice(["all", *update_target.get_update_choices()]),
+        ),
+    ] = ["all"],  # noqa: B006
+    dry_run: Annotated[bool, typer.Option("-d", "--dry-run", help="Do not actually change versions.")] = False,
+) -> None:
+    """Update components versions."""
     if "all" in components:
         components = update_target.get_update_choices()
 
     update_target.update(components, dry_run=dry_run)
 
 
-@cli.command(help="Regenerate gnome settings")
+@app.command()
 def gnome() -> None:
+    """Regenerate gnome settings."""
     gnome_target.gnome_config()
 
 
-@cli.command(help="Generate ansible roles dependency graph.")
-@click.option("-s", "--silent", is_flag=True, help="Do not open graph in image viewer.")
-def graph(*, silent: bool) -> None:
+@app.command()
+def graph(
+    *,
+    silent: Annotated[bool, typer.Option("-s", "--silent", help="Do not open graph in image viewer.")] = False,
+) -> None:
+    """Generate ansible roles dependency graph."""
     roles_graph_target.generate_png(view=silent)
 
 
-@cli.command(help="Generate random password.")
-@click.option(
-    "-a",
-    "--alphabet",
-    type=str,
-    default=string.ascii_lowercase,
-    help="Password alphabet.",
-)
-@click.option("-l", "--length", type=int, default=24, help="Password length.")
-@click.option(
-    "-o",
-    "--output",
-    type=click.Path(dir_okay=False, writable=True, executable=False, path_type=Path),
-    default=utils.ARTIFACTS_PATH / "password.txt",
-    help="Where to store generated password.",
-)
-@click.option("-n", "--number", type=int, default=1, help="Number of passwords to generate.")
-def password(*, alphabet: str, length: int, output: Path, number: int) -> None:
+@app.command()
+def password(
+    *,
+    alphabet: Annotated[
+        str,
+        typer.Option("-a", "--alphabet", help="Password alphabet."),
+    ] = string.ascii_lowercase,
+    length: Annotated[int, typer.Option("-l", "--length", help="Password length.")] = 24,
+    output: Annotated[
+        Path,
+        typer.Option("-o", "--output", help="Where to store generated password.", dir_okay=False, writable=True),
+    ] = utils.ARTIFACTS_PATH / "password.txt",
+    number: Annotated[int, typer.Option("-n", "--number", help="Number of passwords to generate.")] = 1,
+) -> None:
+    """Generate random password."""
     passwords_str = "".join(secrets.choice(alphabet) for _ in range(length * number))
     passwords = [passwords_str[i : i + length] + "\n" for i in range(0, len(passwords_str), length)]
     output.parent.mkdir(parents=True, exist_ok=True)
+    output.touch(0o600)
     with output.open("a") as output_file:
         output_file.writelines(passwords)
     _logger.info(f"Password saved to the end of {output} file")
 
 
-@cli.command(name="omp-themes", help="Choose oh-my-posh theme candidates.")
-@click.option("-a", "--from-all", is_flag=True, help="Choose from all themes, not from already chosen candidates.")
-def omp_themes(*, from_all: bool) -> None:
+@app.command(name="omp-themes")
+def omp_themes(
+    *,
+    from_all: Annotated[
+        bool, typer.Option("-a", "--from-all", help="Choose from all themes, not from already chosen candidates.")
+    ] = False,
+) -> None:
+    """Choose oh-my-posh theme candidates."""
     oh_my_posh.choose(from_all=from_all)
-
-
-def main() -> None:
-    utils.setup_logger()
-    cli(max_content_width=shutil.get_terminal_size(fallback=(120, 24)).columns)

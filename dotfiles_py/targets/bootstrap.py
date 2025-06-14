5844efe745fa2442e64b354a24fa1e10b3d45245
Nikita Rudenko <rudenkornk@gmail.com>
2025-06-14 15:39:38 +0300

    refactor(scripts): fix check-bootstrap

--- dotfiles_py/targets/bootstrap.py
+++ dotfiles_py/targets/bootstrap.py
@@ -9,10 +9,15 @@ def check_bootstrap(image: str) -> None:
     @makelike(
         ARTIFACTS_PATH / norm_image / "bootstrap_control_node",
         REPO_PATH / "main.sh",
+        REPO_PATH / "pyproject.toml",
+        REPO_PATH / "uv.lock",
+        REPO_PATH / "roles" / "manifest" / "vars" / "main.yaml",
         Path(__file__),
         auto_create=True,
     )
     def run_check_bootstrap(_: Path, sources: list[Path]) -> None:
+        venv = ARTIFACTS_PATH / norm_image / "venv"
+        venv.mkdir(parents=True, exist_ok=True)
         run_shell(
             [
                 "podman",
@@ -22,6 +27,8 @@ def check_bootstrap(image: str) -> None:
                 "--tty",
                 "--volume",
                 f"{REPO_PATH}:{REPO_PATH}",
+                "--volume",
+                f"{venv}:{REPO_PATH}/.venv",
                 "--workdir",
                 REPO_PATH,
                 image,

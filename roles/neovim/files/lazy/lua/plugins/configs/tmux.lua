d3d18da559763535392121f1656021b6146c5e86
Nikita Rudenko <rudenkornk@gmail.com>
2025-09-14 19:50:11 +0300

    refactor(tmux): amend tmux-nvim keymaps

--- roles/neovim/files/lazy/lua/plugins/configs/tmux.lua
+++ roles/neovim/files/lazy/lua/plugins/configs/tmux.lua
@@ -47,103 +47,59 @@ M.opts = {
   },
 }
 
+local function tmux_move(direction, key)
+  return {
+    key,
+    function()
+      local tmux = require("tmux")
+      if direction == "left" then
+        tmux.move_left()
+      elseif direction == "right" then
+        tmux.move_right()
+      elseif direction == "bottom" then
+        tmux.move_bottom()
+      elseif direction == "top" then
+        tmux.move_top()
+      end
+    end,
+    desc = "Move cursor to the " .. direction .. " window",
+    mode = { "c", "n", "t", "v", "i" },
+  }
+end
+
+local function tmux_resize(direction, key, size)
+  return {
+    key,
+    function()
+      local tmux = require("tmux")
+      if direction == "left" then
+        tmux.resize_left(size)
+      elseif direction == "right" then
+        tmux.resize_right(size)
+      elseif direction == "bottom" then
+        tmux.resize_bottom(size)
+      elseif direction == "top" then
+        tmux.resize_top(size)
+      end
+    end,
+    desc = "Resize window to the " .. direction .. " by " .. size,
+    mode = { "c", "n", "t", "v", "i" },
+  }
+end
+
 M.keys = {
-  {
-    "<C-h>",
-    function()
-      require("tmux").move_left()
-    end,
-    desc = "Move cursor to the left window",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<C-j>",
-    function()
-      require("tmux").move_bottom()
-    end,
-    desc = "Move cursor to the bottom window",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<C-k>",
-    function()
-      require("tmux").move_top()
-    end,
-    desc = "Move cursor to the top window",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<C-l>",
-    function()
-      require("tmux").move_right()
-    end,
-    desc = "Move cursor to the right window",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x>h",
-    function()
-      require("tmux").resize_left(20)
-    end,
-    desc = "Resize window to the left",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x>j",
-    function()
-      require("tmux").resize_bottom(20)
-    end,
-    desc = "Resize window to the bottom",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x>k",
-    function()
-      require("tmux").resize_top(20)
-    end,
-    desc = "Resize window to the top",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x>l",
-    function()
-      require("tmux").resize_right(20)
-    end,
-    desc = "Resize window to the right",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x><S-h>",
-    function()
-      require("tmux").resize_left(1)
-    end,
-    desc = "Resize window to the left",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x><S-j>",
-    function()
-      require("tmux").resize_bottom(1)
-    end,
-    desc = "Resize window to the bottom",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x><S-k>",
-    function()
-      require("tmux").resize_top(1)
-    end,
-    desc = "Resize window to the top",
-    mode = { "c", "n", "t", "v", "i" },
-  },
-  {
-    "<A-x><S-l>",
-    function()
-      require("tmux").resize_right(1)
-    end,
-    desc = "Resize window to the right",
-    mode = { "c", "n", "t", "v", "i" },
-  },
+  tmux_move("left", "<C-h>"),
+  tmux_move("right", "<C-l>"),
+  tmux_move("bottom", "<C-j>"),
+  tmux_move("top", "<C-k>"),
+  tmux_resize("left", "<A-x>h", 15),
+  tmux_resize("right", "<A-x>l", 15),
+  tmux_resize("bottom", "<A-x>j", 10),
+  tmux_resize("top", "<A-x>k", 10),
+  tmux_resize("left", "<A-x><S-h>", 1),
+  tmux_resize("right", "<A-x><S-l>", 1),
+  tmux_resize("bottom", "<A-x><S-j>", 1),
+  tmux_resize("top", "<A-x><S-k>", 1),
 }
 
 return M

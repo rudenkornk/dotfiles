f262b65fd165802d717e8cbdec17f21f552288b1
Nikita Rudenko <rudenkornk@gmail.com>
2025-09-23 12:28:25 +0300

    refactor(neovim): make render-markdown less flickery

--- roles/neovim/files/lazy/lua/plugins/init.lua
+++ roles/neovim/files/lazy/lua/plugins/init.lua
@@ -68,10 +68,18 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
+      checkbox = {
+        enabled = true,
+        -- `bullet = true` and `right_pad = 2` makes line same width rendered and unrendered.
+        bullet = true,
+        right_pad = 2,
+      },
       -- Prevent rendered and normal versions to be different in lines,
       -- which makes document "flicker" when switching between them.
       code = { border = "thick" },
       pipe_table = { style = "normal" },
+      link = { enabled = false },
+      win_options = { conceallevel = { default = 0, rendered = 0 } },
       file_types = { "markdown", "Avante" },
     },
     ft = { "markdown", "Avante" },

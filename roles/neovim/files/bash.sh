0712a4741f1afeb3ffa43e7c82fdd25cef4656ca
Nikita Rudenko <rudenkornk@gmail.com>
2025-09-28 16:38:26 +0300

    refactor(secrets): add more AI API keys

--- roles/neovim/files/bash.sh
+++ roles/neovim/files/bash.sh
@@ -11,6 +11,13 @@ alias vd="nvim -d"
 if [[ -n "$NVIM" && -n "$MYVIMRC" ]]; then
   unset HTTP_PROXY
   unset HTTPS_PROXY
+  unset http_proxy
+  unset https_proxy
+
+  unset OPENAI_API_KEY
+  unset GEMINI_API_KEY
+  unset CODESTRAL_API_KEY
+  unset DEEPSEEK_API_KEY
   unset TAVILY_API_KEY
   unset MORPH_API_KEY
   unset CORP_LLM_API_KEY

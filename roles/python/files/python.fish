8bbc6cbb8310bfc4dc35b08fb6e4c31f8750287c
Nikita Rudenko <rudenkornk@gmail.com>
2025-10-05 17:05:02 +0300

    refactor(python): amend fish config

--- roles/python/files/python.fish
+++ roles/python/files/python.fish
@@ -1,8 +1,6 @@
 set current "$(pwd)"
 set host $(hostname)
 
-set -a venvs ".makepy/artifacts/venv/$(hostname)/bin/activate.fish"
-set -a venvs ".makepy/artifacts/uv-$(hostname)/bin/activate.fish"
 set -a venvs ".venv/bin/activate.fish"
 set -a venvs "venv/bin/activate.fish"
 
@@ -10,6 +8,7 @@ while string match --quiet "$HOME*" "$current"
   set activate "$current/.makepy/artifacts/venv/$(hostname)/bin/activate.fish"
   for candidate in $venvs
     if test -f $current/$candidate
+      set --export VIRTUAL_ENV_DISABLE_PROMPT 1
       source $current/$candidate
       return
     end

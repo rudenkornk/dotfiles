1e9fb9378ac2afa9680f72ac583404d6dd08dc0b
Nikita Rudenko <rudenkornk@gmail.com>
2025-10-08 09:03:31 +0300

    NIX

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -1,124 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "rudenkornk";
-  home.homeDirectory = "/home/rudenkornk";
+  home = {
+    packages = with pkgs; [ hello ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+    username = "rudenkornk";
+    homeDirectory = "/home/rudenkornk";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    acpi
-    ansible
-    ansible-lint
-    automake
-    bzip2
-    ccache
-    cmake
-    curl
-    docker
-    dos2unix
-    file
-    flameshot
-    flex
-    gcc14
-    gdb
-    gh
-    git
-    git-secret
-    gnumake
-    gnupg
-    gnutar
-    go
-    graphviz
-    gzip
-    htop-vim
-    iptables
-    jdk23
-    lazygit
-    lftp
-    #llvmPackages_19.clangWithLibcAndBasicRtAndLibcxx
-    lsb-release
-    mr
-    neofetch
-    nettools
-    ninja
-    ntp
-    patch
-    pkg-config
-    podman
-    rsync
-    tcl
-    tmux
-    tzdata
-    unar
-    unrar
-    unzip
-    unzip
-    valgrind
-    vcpkg
-    wget
-    xsel
-    xz
-    zip
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    stateVersion = "25.05";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/rudenkornk/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = { EDITOR = "nvim"; };
-
-  # Let Home Manager install and manage itself.
+  # Let home-manager install and manage itself.
   programs.home-manager.enable = true;
 }

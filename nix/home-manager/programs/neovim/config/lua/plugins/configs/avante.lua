3cae4ec38fba7a7bbecf0f71bceeabe01dcee568
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-02 16:15:14 +0300

    refactor(neovim): use lazy Avante extra instead of manual integration

--- nix/home-manager/programs/neovim/config/lua/plugins/configs/avante.lua
+++ nix/home-manager/programs/neovim/config/lua/plugins/configs/avante.lua
@@ -1,9 +1,5 @@
 M = {}
 
--- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
--- ⚠️ must add this setting! ! !
-M.build = "make"
-
 M.opts = {
   provider = os.getenv("USERKIND") == "corp" and "corp" or "copilot",
   providers = {
@@ -16,18 +12,6 @@ M.opts = {
       api_key_name = "CORP_LLM_API_KEY",
       model = "Qwen3-Coder-30B-A3B-Instruct-FP8",
     },
-  },
-  input = {
-    provider = "snacks",
-    provider_opts = {
-      -- Additional snacks.input options
-      title = "Avante Input",
-      icon = "󰇀",
-    },
-  },
-  selector = {
-    provider = "snacks",
-    provider_opts = {},
   },
   mappings = {
     sidebar = {

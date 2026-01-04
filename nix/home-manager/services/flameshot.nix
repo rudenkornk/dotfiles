9c9dd5d4a0d5ca8a436f95fde0c5c18a49addbb1
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-04 22:51:32 +0300

    refactor(flameshot): improve screenshotting
    * Disable sound
    * Fix not-copying screenshots to clipboard
    * Fix not-capturing all monitors
    * Improve shortcuts
    * Other minor improvements

--- nix/home-manager/services/flameshot.nix
+++ nix/home-manager/services/flameshot.nix
@@ -1,13 +1,32 @@
-_: {
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    wl-clipboard
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal
+  ];
+
   services.flameshot = {
     enable = true;
     settings = {
       General = {
-        contrastOpacity = 119;
+        contrastOpacity = 84;
+        copyOnDoubleClick = true;
+        disabledTrayIcon = true;
         drawThickness = 15;
+        filenamePattern = "%Y-%b-%d-%Hh%Mm%Ss";
+        insecurePixelate = true;
+        predefinedColorPaletteLarge = true;
+        saveLastRegion = true;
+        showAbortNotification = false;
         showDesktopNotification = false;
+        showStartupLaunchMessage = false;
         startupLaunch = true;
         undoLimit = 999;
+      };
+      Shortcuts = {
+        TYPE_ACCEPT = "Ctrl+c";
+        TYPE_CIRCLECOUNT = "c";
       };
     };
   };

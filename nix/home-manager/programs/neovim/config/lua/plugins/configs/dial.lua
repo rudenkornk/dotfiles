8c991dba4b0efd5ecc8fa0cff8a83b9d9d316479
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-06 04:44:12 +0300

    refactor(neovim): add more pairs to dial

--- nix/home-manager/programs/neovim/config/lua/plugins/configs/dial.lua
+++ nix/home-manager/programs/neovim/config/lua/plugins/configs/dial.lua
@@ -27,6 +27,7 @@ M.config = function()
   local boolean_ops = { "and", "or" }
   local on_off = { "on", "off" }
   local yes_no = { "yes", "no" }
+  local included_excluded = { "included", "excluded" }
   local numbers = {
     "one",
     "two",
@@ -84,6 +85,7 @@ M.config = function()
   local boolean_ops_augend = create_constants(boolean_ops, true, true)
   local on_off_augend = create_constants(on_off, true, true)
   local yes_no_augend = create_constants(yes_no, true, true)
+  local included_excluded_augend = create_constants(included_excluded, true, true)
   local number_augend = create_constants(numbers, false, false)
   local ordinal_augend = create_constants(ordinals, false, false)
   local weekday_augend = create_constants(weekdays, false, true)
@@ -104,6 +106,7 @@ M.config = function()
       unpack(boolean_ops_augend),
       unpack(on_off_augend),
       unpack(yes_no_augend),
+      unpack(included_excluded_augend),
       unpack(number_augend),
       unpack(ordinal_augend),
       unpack(weekday_augend),

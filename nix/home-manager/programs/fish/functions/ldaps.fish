fe3412d2ff9126638faf28c3845dafa153db373d
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-20 20:39:58 +0300

    refactor(secrets): amend corp secrets

--- nix/home-manager/programs/fish/functions/ldaps.fish
+++ nix/home-manager/programs/fish/functions/ldaps.fish
@@ -1,6 +1,6 @@
 set ldap_args $(\
-  @jq@/bin/jq -r '" -D " + .user + " -w " + .password + " -H " + .server + " -b " + .domain' \
-  $(@sops-cached@/bin/sops-cached  @ldap_auth@) |\
+  @jq@/bin/jq -r '" -D " + .email + " -w " + .password + " -H " + .ldap_server + " -b " + .ldap_domain' \
+  $(@sops-cached@/bin/sops-cached  @corp_auth@) |\
   string split " "\
 )
 ldapsearch $ldap_args $argv

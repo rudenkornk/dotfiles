9e3efde62d413918724e82a6fd631e821fb0f9dc
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-20 21:36:20 +0300

    refactor(fish): amend ldaps config

--- nix/home-manager/programs/fish/functions/ldaps.fish
+++ nix/home-manager/programs/fish/functions/ldaps.fish
@@ -1,6 +1,10 @@
+# Stupid `|| return 1` due to missing `set -e` support in fish functions.
+# See https://github.com/fish-shell/fish-shell/issues/510
+
+set auth_file $(@sops-cached@/bin/sops-cached  @corp_auth@) || return 1
 set ldap_args $(\
-  @jq@/bin/jq -r '" -D " + .email + " -w " + .password + " -H " + .ldap_server + " -b " + .ldap_domain' \
-  $(@sops-cached@/bin/sops-cached  @corp_auth@) |\
-  string split " "\
-)
-ldapsearch $ldap_args $argv
+  @jq@/bin/jq --exit-status -r '"-D " + .email + " -w " + .password + " -H " + .ldap_server + " -b " + .ldap_domain' \
+  $auth_file | string split " "\
+) || { echo "Failed to get LDAP credentials." >&2; return 1 }
+
+@openldap@/bin/ldapsearch $ldap_args $argv

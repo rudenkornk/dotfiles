6417e167f48ffb753797cbba24c1bb359c6ebf76
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-25 19:18:36 +0300

    refactor(locallib): amend sops-cached util
    Support custom secrets filenames

--- nix/nixpkgs/overlays/sops-cached/sops-cached.sh
+++ nix/nixpkgs/overlays/sops-cached/sops-cached.sh
@@ -1,8 +1,15 @@
 # shellcheck shell=bash
 
 file="$1"
+suffix="${2:-}"
 
 decrypted=/run/user/"$(id --user)"/secrets/$(basename "$file")_decrypted
+if [[ -n "$suffix" ]]; then
+  # Suffix allows making a specific name for a secret.
+  # For example some programs may require fixed names for their configs.
+  decrypted="${decrypted}/${suffix}"
+fi
+
 if [[ -f "$decrypted" ]]; then
   echo "$decrypted"
   exit

02388e50736442ca01a303f0f4be7970dd3e1170
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-29 11:30:29 +0300

    refactor(programs): add kubeadm package

--- nix/home-manager/programs.nix
+++ nix/home-manager/programs.nix
@@ -101,6 +101,7 @@
     docker-compose
     docker-machine-kvm2 # Required for `minikube --driver kvm2`, see https://github.com/kubernetes/minikube/issues/6023#issuecomment-2103782263
     kubectl
+    kubernetes
     minikube
     podman
     qemu_full

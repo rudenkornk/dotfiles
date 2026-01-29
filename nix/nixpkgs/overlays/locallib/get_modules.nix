671074ae0ad141f3f05a161bbd118f1091a04035
Nikita Rudenko <rudenkornk@gmail.com>
2026-01-29 18:44:39 +0300

    refactor(locallib): add get_modules' func

--- nix/nixpkgs/overlays/locallib/get_modules.nix
+++ nix/nixpkgs/overlays/locallib/get_modules.nix
@@ -1,29 +1,3 @@
 _: path:
 
-let
-  inherit (builtins)
-    attrNames
-    filter
-    listToAttrs
-    readDir
-    stringLength
-    substring
-    ;
-
-  hasSuffix =
-    suffix: str:
-    let
-      sufLen = stringLength suffix;
-      strLen = stringLength str;
-    in
-    sufLen <= strLen && substring (strLen - sufLen) sufLen str == suffix;
-
-  entries = readDir path;
-  nixFiles = filter (name: entries.${name} == "regular" && hasSuffix ".nix" name) (attrNames entries);
-in
-listToAttrs (
-  map (name: {
-    name = substring 0 (stringLength name - 4) name;
-    value = path + "/${name}";
-  }) nixFiles
-)
+builtins.attrValues (import ./get_modules_map.nix null path)

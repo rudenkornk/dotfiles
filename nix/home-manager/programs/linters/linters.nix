8733469ac226181bed780961d938a40832ec24f1
Nikita Rudenko <rudenkornk@gmail.com>
2026-02-02 21:02:41 +0300

    refactor(programs): utilize homefiles util

--- nix/home-manager/programs/linters/linters.nix
+++ nix/home-manager/programs/linters/linters.nix
@@ -35,18 +35,9 @@
     yamlfmt
     yamllint
   ];
-
-  home.file = {
-    "${config.xdg.configHome}/luacheck/.luacheckrc".source = ./configs/.config/luacheck/.luacheckrc;
-    "${config.xdg.configHome}/yamlfmt/.yamlfmt".source = ./configs/.config/yamlfmt/.yamlfmt;
-    "${config.xdg.configHome}/yamllint/config".source = ./configs/.config/yamllint/config;
-    "${config.xdg.configHome}/black".source = ./configs/.config/black;
-    "${config.xdg.configHome}/pycodestyle".source = ./configs/.config/pycodestyle;
-    "${config.xdg.configHome}/pylintrc".source = ./configs/.config/pylintrc;
-    ".groovylintrc.json".source = ./configs/.groovylintrc.json;
-    ".isort.cfg".source = ./configs/.isort.cfg;
-    ".markdownlint.yaml".source = ./configs/.markdownlint.yaml;
-    ".pydocstyle".source = ./configs/.pydocstyle;
+  home.file = pkgs.locallib.homefiles {
+    inherit (config) xdg;
+    path = ./configs;
   };
 
   programs.mypy = {

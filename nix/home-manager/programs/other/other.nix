98a713626eadfb4fec49ff0c1af68f41b5f97ef7
Nikita Rudenko <rudenkornk@gmail.com>
2026-02-24 21:18:11 +0300

    refactor(programs): add faster tldr tool

--- nix/home-manager/programs/other/other.nix
+++ nix/home-manager/programs/other/other.nix
@@ -2,15 +2,23 @@
 
 # Other useful tools.
 {
-  home.packages = with pkgs; [
-    asciinema
-    dconf
-    dconf-editor
-    dos2unix
-    gh
-    hyperfine
-    tldr
-    wiki-tui
-    xh
-  ];
+  home = {
+    packages = with pkgs; [
+      asciinema
+      dconf
+      dconf-editor
+      dos2unix
+      gh
+      hyperfine
+      wiki-tui
+      xh
+    ];
+  };
+
+  programs = {
+    tealdeer = {
+      enable = true;
+      settings.updates.auto_update = true;
+    };
+  };
 }

c8674f81e4fbfd4500024c034df7989c093703d9
Nikita Rudenko <rudenkornk@gmail.com>
2026-03-31 17:19:40 +0300

    refactor(neovim): amend lualine setup

--- nix/home-manager/programs/editors/neovim/config/lua/plugins/configs/lualine.lua
+++ nix/home-manager/programs/editors/neovim/config/lua/plugins/configs/lualine.lua
@@ -1,21 +1,19 @@
 local M = {}
 
-M.opts = {
-  sections = {
-    lualine_a = {
-      { "mode" },
-      {
-        function()
-          if vim.o.iminsert == 1 then
-            return "ru"
-          else
-            return "en"
-          end
-        end,
-      },
-    },
-    lualine_z = {},
-  },
-}
+M.opts = function(_, opts)
+  table.insert(opts.sections.lualine_a, {
+    function()
+      if vim.o.iminsert == 1 then
+        return "ru"
+      else
+        return "en"
+      end
+    end,
+  })
+  -- Assign cursor position to the last segment.
+  -- Previously last segment displayed datetime, which is redundant in `tmux` context.
+  opts.sections.lualine_z = opts.sections.lualine_y
+  opts.sections.lualine_y = {}
+end
 
 return M

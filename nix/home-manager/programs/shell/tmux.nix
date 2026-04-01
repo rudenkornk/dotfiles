172baeea4401af79370a752e060f799a7d519517
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-01 14:09:37 +0300

    refactor(tmux): use ukiyo plugin from unstable nixpkgs

--- nix/home-manager/programs/shell/tmux.nix
+++ nix/home-manager/programs/shell/tmux.nix
@@ -26,52 +26,46 @@
 
     prefix = "C-s";
     extraConfig = builtins.readFile ./tmux/tmux.conf;
-    plugins =
-      let
-        # TODO: remove this when tmux-ukiyo is updated in nixpkgs.
-        # https://github.com/NixOS/nixpkgs/pull/485092
-        ukiyo-upd = pkgs.tmuxPlugins.mkTmuxPlugin {
-          pluginName = "ukiyo";
-          version = "1.0.0";
-          src = inputs.tmux_plugin_ukiyo;
-        };
-      in
-      with pkgs.tmuxPlugins;
-      [
-        {
-          # Quick copy pane contents with tmux-fingers,
-          # Alternatives to tmux-fingers:
-          # CrispyConductor/tmux-copy-toolkit: too much key bindings, hard to configure, poor UX, quickcopy mode enables weird highlighting.
-          # abhinav/tmux-fastcopy: works great, but flashes screen.
-          # fcsonline/tmux-thumbs: flashes screen.
-          # tmux-plugins/tmux-copycat: too much key bindings, does not have easy mode.
-          # tmux-plugins/tmux-urlview: only urls.
-          plugin = fingers;
-          extraConfig = ''
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        # Quick copy pane contents with tmux-fingers,
+        # Alternatives to tmux-fingers:
+        # CrispyConductor/tmux-copy-toolkit: too much key bindings, hard to configure, poor UX, quickcopy mode enables weird highlighting.
+        # abhinav/tmux-fastcopy: works great, but flashes screen.
+        # fcsonline/tmux-thumbs: flashes screen.
+        # tmux-plugins/tmux-copycat: too much key bindings, does not have easy mode.
+        # tmux-plugins/tmux-urlview: only urls.
+        plugin = fingers;
+        extraConfig = # tmux
+          ''
             set -g @fingers-key 'C-f'
           '';
-        }
-        {
-          plugin = fzf-tmux-url;
-          extraConfig = ''
+      }
+      {
+        plugin = fzf-tmux-url;
+        extraConfig = # tmux
+          ''
             set -g @fzf-url-fzf-options '-w 50% -h 50%'
           '';
-        }
-        {
-          plugin = jump;
-          extraConfig = ''
+      }
+      {
+        plugin = jump;
+        extraConfig = # tmux
+          ''
             set -g @jump-key 'C-d'
           '';
-        }
-        {
-          plugin = yank;
-          extraConfig = ''
+      }
+      {
+        plugin = yank;
+        extraConfig = # tmux
+          ''
             set -g @yank_action 'copy-pipe' # or 'copy-pipe-and-cancel' for the default
           '';
-        }
-        {
-          plugin = ukiyo-upd;
-          extraConfig = ''
+      }
+      {
+        plugin = pkgs.unstable.tmuxPlugins.ukiyo;
+        extraConfig = # tmux
+          ''
             set -g @ukiyo-theme 'tokyonight/night'
 
             set -g @ukiyo-plugins "network-bandwidth disk-usage cpu-usage ram-usage ssh-session"
@@ -96,8 +90,8 @@
             set -g @ukiyo-show-location false
             set -g @ukiyo-show-ssh-session-port true
           '';
-        }
-      ];
+      }
+    ];
   };
 
   programs.fish = {

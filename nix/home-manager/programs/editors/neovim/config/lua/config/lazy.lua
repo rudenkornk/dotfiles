3ba48562780999749db23d90496472b0f07662f5
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-01 22:11:24 +0300

    feat(neovim): fetch treesitter grammars and most of plugins from nix

--- nix/home-manager/programs/editors/neovim/config/lua/config/lazy.lua
+++ nix/home-manager/programs/editors/neovim/config/lua/config/lazy.lua
@@ -1,19 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" }, -- Add LazyVim and import its plugins.
@@ -27,6 +11,11 @@ require("lazy").setup({
   },
   checker = {
     enabled = false, -- Do not check for plugin updates.
+  },
+  dev = {
+    path = require("config.nix_managed_plugins"), -- Reuse files from pkgs.vimPlugins.*
+    patterns = { "." },
+    fallback = true, -- If a plugin isn't found in the linkFarm, Lazy will fall back to downloading it.
   },
   performance = {
     rtp = {

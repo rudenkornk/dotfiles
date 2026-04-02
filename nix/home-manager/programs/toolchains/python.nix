4da2ead59ff391d219590e6380d1a8657ee8e5e6
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-02 19:38:30 +0300

    refactor(python): add python 3.10

--- nix/home-manager/programs/toolchains/python.nix
+++ nix/home-manager/programs/toolchains/python.nix
@@ -3,7 +3,8 @@
 # Compilers, interpreters, build systems & language processors.
 {
   home.packages = with pkgs; [
-    (lib.setPrio 10 python3)
+    (lib.setPrio 9 python3)
+    (lib.setPrio 10 python310)
     (lib.setPrio 11 python311)
     (lib.setPrio 12 python312)
     (lib.setPrio 13 python313)

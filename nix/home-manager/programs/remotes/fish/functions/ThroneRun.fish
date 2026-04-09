203bfd2bcd234a7898ef22280bead5ea92222653
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-10 02:23:51 +0300

    refactor(remotes): amend throne launcher

--- nix/home-manager/programs/remotes/fish/functions/ThroneRun.fish
+++ nix/home-manager/programs/remotes/fish/functions/ThroneRun.fish
@@ -3,6 +3,11 @@ sudo echo ""
 
 # Redirect both stdout and stderr to a log file.
 # Otherwise, `nohup` will create unnecessary `nohup.out` in cwd.
-nohup sudo @throne@/bin/Throne $args &>/tmp/Throne_$USER.log &
+nohup sudo \
+    DISPLAY=$DISPLAY \
+    WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+    XAUTHORITY=$XAUTHORITY \
+    XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    @throne@/bin/Throne $args &>/tmp/Throne_$USER.log &
 
 disown

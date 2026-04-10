e62f9b192c5f92807b1f46de4d0aebf41334f056
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-10 14:11:14 +0300

    feat(hosts): add info about monitors

--- nix/hosts/thinkpad.nix
+++ nix/hosts/thinkpad.nix
@@ -72,4 +72,8 @@
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
+
+  monitors = {
+    niri = { };
+  };
 }

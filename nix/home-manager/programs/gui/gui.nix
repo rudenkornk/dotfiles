7660df264ee4c21aa5aa7250099c88bec75ac2c3
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-11 01:29:10 +0300

    feat(gui): add custom cursor

--- nix/home-manager/programs/gui/gui.nix
+++ nix/home-manager/programs/gui/gui.nix
@@ -14,6 +14,22 @@
       inherit (config) xdg;
       path = ./configs;
     };
+
+    pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Original-Amber";
+      size = 32;
+    };
+  };
+
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Original-Amber"; # Keep in sync with home.pointerCursor.name.
+    };
   };
 
   programs = {

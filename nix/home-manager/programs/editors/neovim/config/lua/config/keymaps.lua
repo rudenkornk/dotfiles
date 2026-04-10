ab5dad29352d60bd905c53fe2a3d6df4de3e20d9
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-10 14:10:33 +0300

    feat(gnome): increase keyboard repeat rate

--- nix/home-manager/programs/editors/neovim/config/lua/config/keymaps.lua
+++ nix/home-manager/programs/editors/neovim/config/lua/config/keymaps.lua
@@ -107,8 +107,8 @@ vim.keymap.set(
   function() end,
   { silent = true, desc = "Service keymap to disable neovim suspension" }
 )
-vim.keymap.set({ "n", "x" }, "<C-e>", "4<C-e>", { silent = true, desc = "Scroll up" })
-vim.keymap.set({ "n", "x" }, "<C-y>", "4<C-y>", { silent = true, desc = "Scroll down" })
+vim.keymap.set({ "n", "x" }, "<C-e>", "3<C-e>", { silent = true, desc = "Scroll up" })
+vim.keymap.set({ "n", "x" }, "<C-y>", "3<C-y>", { silent = true, desc = "Scroll down" })
 vim.keymap.set({ "x" }, "<leader>vs", ":sort i<CR>", { desc = "Sort selected lines" })
 vim.keymap.set({ "n" }, "<C-q>", "a<C-^><ESC>", { desc = "Toggle keyboard layout" })
 

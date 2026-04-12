570769a2017ceb2ff4c46f76a4c8bdddb6a07274
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-12 18:02:08 +0300

    refactor(nix): split sections

--- nix/home-manager/programs/nix/nix.nix
+++ nix/home-manager/programs/nix/nix.nix
@@ -2,21 +2,24 @@
 
 # Nix-related tools.
 {
-  home.packages = with pkgs; [
-    cachix
-    dconf2nix
-    home-manager
-    nh
-    nix-diff
-    nix-index
-    nix-melt
-    nix-output-monitor
-    nix-top
-    nix-tree
-  ];
+  home = {
+    packages = with pkgs; [
+      cachix
+      dconf2nix
+      home-manager
+      nix-diff
+      nix-index
+      nix-melt
+      nix-output-monitor
+      nix-top
+      nix-tree
+    ];
+  };
 
-  programs.nix-search-tv = {
-    enable = true;
-    enableTelevisionIntegration = true;
+  programs = {
+    nix-search-tv = {
+      enable = true;
+      enableTelevisionIntegration = true;
+    };
   };
 }

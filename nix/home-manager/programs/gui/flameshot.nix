ff7cd07eaf7fe72313f14439ac4c893322205676
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-13 12:50:19 +0300

    refactor(gui): move flameshot settings to native config

--- nix/home-manager/programs/gui/flameshot.nix
+++ nix/home-manager/programs/gui/flameshot.nix
@@ -8,26 +8,5 @@
 
   services.flameshot = {
     enable = true;
-    settings = {
-      General = {
-        contrastOpacity = 84;
-        copyOnDoubleClick = true;
-        disabledTrayIcon = true;
-        drawThickness = 15;
-        filenamePattern = "%Y-%b-%d-%Hh%Mm%Ss";
-        insecurePixelate = true;
-        predefinedColorPaletteLarge = true;
-        saveLastRegion = true;
-        showAbortNotification = false;
-        showDesktopNotification = false;
-        showStartupLaunchMessage = false;
-        startupLaunch = true;
-        undoLimit = 999;
-      };
-      Shortcuts = {
-        TYPE_ACCEPT = "Ctrl+c";
-        TYPE_CIRCLECOUNT = "c";
-      };
-    };
   };
 }

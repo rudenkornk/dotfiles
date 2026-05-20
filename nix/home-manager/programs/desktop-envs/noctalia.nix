d1862392e151df7bc7eec32d399d3b7cedf6f32f
Nikita Rudenko <rudenkornk@gmail.com>
2026-05-20 15:54:04 +0300

    feat(desktop-envs): move some gui apps to desktop-envs category

---
+++ nix/home-manager/programs/desktop-envs/noctalia.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  config,
+  lib,
+  host,
+  ...
+}:
+
+let
+  inherit (builtins) fromJSON readFile;
+  toJson = (pkgs.formats.json { }).generate;
+in
+{
+  home = {
+    packages = with pkgs; [
+      unstable.noctalia-shell
+      # https://docs.noctalia.dev/v4/getting-started/installation/#dependencies-explained
+      bluez # Bluetooth support.
+      brightnessctl # Brightness control.
+      cliphist # Clipboard history support.
+      ddcutil # Brightness control for external monitors.
+      evolution-data-server # Calendar events.
+      git # Update checking and the plugin system.
+      imagemagick # Template processing & wallpaper resizing.
+      power-profiles-daemon # Power profile selection.
+      python3 # Template processing & calendar events.
+      upower # Battery state.
+      wlsunset # Night light functionality.
+      xdg-desktop-portal # Screen sharing and file picker functionality.
+    ];
+  };
+
+  xdg = {
+    configFile = {
+      "noctalia/settings.json".source =
+        let
+          main_settings = fromJSON (readFile ./noctalia/settings.json);
+          nix_settings = lib.recursiveUpdate main_settings (host.monitors.noctalia or { });
+        in
+        toJson "noctalia-settings.json" nix_settings;
+    };
+  };
+}
--- nix/home-manager/programs/gui/gui.nix
+++ nix/home-manager/programs/gui/gui.nix
@@ -8,41 +8,11 @@
       prismlauncher
       telegram-desktop
       qbittorrent
-
-      # Screenshot tools.
-      ffmpeg
-      gifski
-      grim
-      imagemagick
-      ksnip
-      satty
-      slurp
-      tesseract
-      translate-shell
-      wl-clipboard
-      wl-screenrec
-      zbar
     ];
 
     file = pkgs.locallib.homefiles {
       inherit (config) xdg;
       path = ./configs;
-    };
-
-    pointerCursor = {
-      gtk.enable = true;
-      x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Original-Amber";
-      size = 32;
-    };
-  };
-
-  gtk = {
-    enable = true;
-    cursorTheme = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Original-Amber"; # Keep in sync with home.pointerCursor.name.
     };
   };
 

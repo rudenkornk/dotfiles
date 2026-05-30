04ed96fcad0529555c41bb693feb87dab6ce8c9f
Nikita Rudenko <rudenkornk@gmail.com>
2026-05-30 23:01:07 +0300

    refactor(shell): reformat yazi.nix and lazygit.nix

--- nix/home-manager/programs/vcs/lazygit.nix
+++ nix/home-manager/programs/vcs/lazygit.nix
@@ -1,28 +1,30 @@
 _: {
-  programs.lazygit = {
-    enable = true;
-    # Disable bash integration, since it is broken in lazygit:
-    #     bash: ~/.bashrc: line 82: syntax error near unexpected token `('
-    #     bash: ~/.bashrc: line 82: `lg() {'
-    # https://github.com/nix-community/home-manager/pull/8670
-    enableBashIntegration = false;
-    enableFishIntegration = true;
-    enableNushellIntegration = true;
-    settings = {
-      gui = {
-        # The number of lines you scroll by when scrolling the main window.
-        scrollHeight = 20;
-      };
-      keybinding = {
-        universal = {
-          prevPage = "<c-u>";
-          nextPage = "<c-d>";
-          scrollUpMain-alt2 = "<c-b>";
-          scrollDownMain-alt2 = "<c-f>";
+  programs = {
+    lazygit = {
+      enable = true;
+      # Disable bash integration, since it is broken in lazygit:
+      #     bash: ~/.bashrc: line 82: syntax error near unexpected token `('
+      #     bash: ~/.bashrc: line 82: `lg() {'
+      # https://github.com/nix-community/home-manager/pull/8670
+      enableBashIntegration = false;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+      settings = {
+        gui = {
+          # The number of lines you scroll by when scrolling the main window.
+          scrollHeight = 20;
         };
-        files = {
-          findBaseCommitForFixup = ""; # Conflicts with universal `C-f`.
-          openStatusFilter = ""; # Conflicts with universal `C-b`.
+        keybinding = {
+          universal = {
+            prevPage = "<c-u>";
+            nextPage = "<c-d>";
+            scrollUpMain-alt2 = "<c-b>";
+            scrollDownMain-alt2 = "<c-f>";
+          };
+          files = {
+            findBaseCommitForFixup = ""; # Conflicts with universal `C-f`.
+            openStatusFilter = ""; # Conflicts with universal `C-b`.
+          };
         };
       };
     };

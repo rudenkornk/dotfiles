9b1cc986f99f8f83798bfdfb0c3ceeb45f868e6f
Nikita Rudenko <rudenkornk@gmail.com>
2026-05-31 17:38:44 +0300

    fix(shell): fix weird <U+...> chars displayed by less
    This affects any `git` and `bat` invocation,
    since both use `less` for pagination.
    
    See also
    https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -6,7 +6,29 @@
     homeDirectory = "/home/${user.username}";
     sessionVariables = {
       USERKIND = user.userkind;
+      # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
       LESSCHARSET = "utf-8";
+      LESSUTFCHARDEF =
+        ""
+        + "23fb-23fe:w,"
+        + "2665:w,"
+        + "2b58:w,"
+        + "e000-e00a:w,"
+        + "e0a0-e0a3:p,"
+        + "e0b0-e0bf:p,"
+        + "e0c0-e0c8:w,"
+        + "e0ca:w,"
+        + "e0cc-e0d7:w,"
+        + "e200-e2a9:w,"
+        + "e300-e3e3:w,"
+        + "e5fa-e6b5:w,"
+        + "e700-e7c5:w,"
+        + "ea60-ec1e:w,"
+        + "ed00-efce:w,"
+        + "f000-f2ff:w,"
+        + "f300-f375:w,"
+        + "f400-f533:w,"
+        + "f0001-f1af0:w";
       QT_SCALE_FACTOR = "1.5";
     };
 

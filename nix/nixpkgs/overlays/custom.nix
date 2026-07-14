63b2991c19e1ad0bd54e2ef4557047041a2778f5
Nikita Rudenko <rudenkornk@gmail.com>
2026-07-14 16:23:07 +0300

    refactor(overlays): move vim-spell to separate module

--- nix/nixpkgs/overlays/custom.nix
+++ nix/nixpkgs/overlays/custom.nix
@@ -58,41 +58,6 @@ _: final: prev: {
 
     playwright-cli = import ./custom/playwright-cli.nix final prev;
 
-    vim-spell =
-      let
-        base = "https://ftp.nluug.nl/vim/runtime/spell";
-        fetch =
-          name: hash:
-          final.fetchurl {
-            url = "${base}/${name}";
-            inherit hash;
-          };
-      in
-      final.stdenv.mkDerivation {
-        name = "vim-spell";
-        srcs = [
-          (fetch "en.utf-8.spl" "sha256-/sq9yUm2o50ywImfolReqyXmPy7QozxK0VEUJjhNMHA=")
-          (fetch "en.utf-8.sug" "sha256-W25eYWVYLS/Xob+kH7zoJCxyR2IixV0XwqorqTPJMuw=")
-          (fetch "ru.utf-8.spl" "sha256-6y0714ogILMLzAp8/r2s6/t6QnWBEU9muIpXeubaxU0=")
-          (fetch "ru.utf-8.sug" "sha256-6r2GForYXVv7gGiAjPeYK6sDdK/CmctJ7MidcWFvOTs=")
-          (fetch "de.utf-8.spl" "sha256-c8cQfqM5hWzb6SHeuSpFk5xN5uucByYdobndGfaDo9E=")
-          (fetch "de.utf-8.sug" "sha256-E9Ds+Shj2J72DNSopesqWhOg6Pm6jRxqvkerqFcUqUg=")
-          (fetch "fr.utf-8.spl" "sha256-q/uXArmNiHwXWs5Y8as5cz3AjQO2dNkU9WNE74bmO2E=")
-          (fetch "fr.utf-8.sug" "sha256-ApS8MrQskLuyhqieI8o3c7fvUO/xq1I7FRPWolxrP1g=")
-          (fetch "la.utf-8.spl" "sha256-MQr0CpSQWbqcnm+Po3UgGV8Zs5/CKTN+pvHcf7+ivqc=")
-          (fetch "es.utf-8.spl" "sha256-ljY3rJJc+KUb8gf6w5LWtMaXlXEdzC1ICbeIRq42e+M=")
-          (fetch "es.utf-8.sug" "sha256-5w80eKplPCrpBQhjKPv/TkO9ZG12U0ZF9QplNEgBvWw=")
-          (fetch "it.utf-8.spl" "sha256-2AczkD6DbVN5DAq4wcLyn2Y8oqd67ns4Guprh2KudBM=")
-          (fetch "it.utf-8.sug" "sha256-4LsXYaeScJJrdaj69PTQ2EDVWis0UY/Y5RKSfCckzko=")
-          (fetch "pl.utf-8.spl" "sha256-FM5QZEzT9p1adhtptZOKbQMYIytctqgB3DTCPaWF5+k=")
-        ];
-        dontUnpack = true;
-        installPhase = ''
-          mkdir -p $out/spell
-          for f in $srcs; do
-            cp "$f" "$out/spell/$(stripHash "$f")"
-          done
-        '';
-      };
+    vim-spell = import ./custom/vim-spell.nix final prev;
   };
 }

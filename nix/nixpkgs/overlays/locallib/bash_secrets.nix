a9453f504b3ee565c312d009f8a78c6a8d48d5fd
Nikita Rudenko <rudenkornk@gmail.com>
2026-07-16 17:36:30 +0300

    refactor(secrets): move corp secrets to separate dir

--- nix/nixpkgs/overlays/locallib/bash_secrets.nix
+++ nix/nixpkgs/overlays/locallib/bash_secrets.nix
@@ -12,6 +12,6 @@ in
     source "$(${sops-cached} ${pkgs.locallib.secrets + /keys.sh.sops})"
   elif [[ "$USERKIND" == "corp" ]]; then
     # shellcheck source=/dev/null
-    source "$(${sops-cached} ${pkgs.locallib.secrets + /corp_keys.sh.sops})"
+    source "$(${sops-cached} ${pkgs.locallib.secrets + /corp/keys.sh.sops})"
   fi
 ''

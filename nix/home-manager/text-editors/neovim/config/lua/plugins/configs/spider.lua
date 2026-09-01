cdf93b48e9cd3c09459e5b17248c7ddd55e42bed
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-01 03:15:31 +0300

    feat(neovim): amend spider config

--- nix/home-manager/text-editors/neovim/config/lua/plugins/configs/spider.lua
+++ nix/home-manager/text-editors/neovim/config/lua/plugins/configs/spider.lua
@@ -1,15 +1,18 @@
 local M = {}
 
 M.opts = {
-  consistentOperatorPending = true,
+  consistentOperatorPending = false,
+  skipInsignificantPunctuation = false,
 }
 
 M.keys = {
   -- Keep Ex commands because Lua callbacks do not preserve dot-repeat.
-  { "w", "<cmd>lua require('spider').motion('w')<cr>", mode = { "n", "o", "x" }, desc = "Next Subword" },
-  { "e", "<cmd>lua require('spider').motion('e')<cr>", mode = { "n", "o", "x" }, desc = "Next Subword End" },
-  { "b", "<cmd>lua require('spider').motion('b')<cr>", mode = { "n", "o", "x" }, desc = "Previous Subword" },
-  { "ge", "<cmd>lua require('spider').motion('ge')<cr>", mode = { "n", "o", "x" }, desc = "Previous Subword End" },
+  { "w", "<cmd>lua require('spider').motion('w')<cr>", mode = { "n", "o", "x" }, desc = "Next subword" },
+  { "e", "<cmd>lua require('spider').motion('e')<cr>", mode = { "n", "o", "x" }, desc = "Next subword end" },
+  { "b", "<cmd>lua require('spider').motion('b')<cr>", mode = { "n", "o", "x" }, desc = "Previous subword" },
+  { "ge", "<cmd>lua require('spider').motion('ge')<cr>", mode = { "n", "o", "x" }, desc = "Previous subword end" },
+
+  { "cw", "c<cmd>lua require('spider').motion('e')<cr>", mode = { "n" }, desc = "Change subword" },
 }
 
 return M

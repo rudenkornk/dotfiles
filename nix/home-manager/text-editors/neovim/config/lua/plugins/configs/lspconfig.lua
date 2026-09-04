c2de88fd45861b34921936fe2ca3e18e22fa3196
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-04 18:32:57 +0300

    s

--- nix/home-manager/text-editors/neovim/config/lua/plugins/configs/lspconfig.lua
+++ nix/home-manager/text-editors/neovim/config/lua/plugins/configs/lspconfig.lua
@@ -1,5 +1,17 @@
 local M = {}
 
+local root_detectors = require("config.root_detectors")
+
+local function cwd_lsp_root(markers)
+  return function(bufnr, on_dir)
+    local root = vim.fs.root(bufnr, markers)
+    -- External definition buffers must not start an LSP server for their containing repository.
+    if root and vim.fs.relpath(root_detectors.cwd_root(bufnr), root) then
+      on_dir(root)
+    end
+  end
+end
+
 M.opts = {
   -- TODO: remove this section entirely once modules support in clangd becomes stable.
   servers = {
@@ -15,6 +27,20 @@ M.opts = {
         "--fallback-style=llvm",
         "--experimental-modules-support",
       },
+    },
+    pyright = {
+      root_dir = cwd_lsp_root({
+        "pyrightconfig.json",
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        ".git",
+      }),
+    },
+    ruff = {
+      root_dir = cwd_lsp_root({ "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" }),
     },
   },
 }

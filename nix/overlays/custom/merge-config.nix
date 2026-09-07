5126b3df052356b3b4dca87d5b1748c1275e96e5
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-07 17:03:16 +0300

    feat(merge-config): support jsonc in merge-config util

--- nix/overlays/custom/merge-config.nix
+++ nix/overlays/custom/merge-config.nix
@@ -2,6 +2,7 @@ final: _prev:
 
 let
   python = final.python3.withPackages (ps: [
+    ps.lark
     ps.ruamel-yaml
     ps.tomlkit
     ps.typer
@@ -9,7 +10,7 @@ let
 in
 final.stdenvNoCC.mkDerivation {
   pname = "merge-config";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = ./scripts/merge-config;
 
@@ -34,11 +35,11 @@ final.stdenvNoCC.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/bin
-    printf '#!${python}/bin/python3\n' > $out/bin/merge-config
-    cat merge-config.py >> $out/bin/merge-config
-    chmod 755 $out/bin/merge-config
-    wrapProgram $out/bin/merge-config \
+    mkdir -p $out/bin $out/libexec/merge-config
+    cp merge-config.py $out/libexec/merge-config/merge-config
+    cp jsonc.py $out/libexec/merge-config/
+    makeWrapper ${python}/bin/python3 $out/bin/merge-config \
+      --add-flags $out/libexec/merge-config/merge-config \
       --prefix PATH : ${final.lib.makeBinPath [ final.custom.sops-cached ]}
     runHook postInstall
   '';

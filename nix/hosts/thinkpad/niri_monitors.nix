8dd740ca969620af3232053dc9abc41cc8859454
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-11 15:59:17 +0300

    feat(noctalia): show bar only on laptop monitor

--- nix/hosts/thinkpad/niri_monitors.nix
+++ nix/hosts/thinkpad/niri_monitors.nix
@@ -9,4 +9,14 @@
     external = false;
     i2c-bus = "/dev/i2c-12";
   };
+
+  "Lenovo Group Limited T27p-30 V5WBV658" = {
+    mode = "3840x2160@59.997";
+    scale = 1.2;
+    position = {
+      x = 160;
+      y = -1440;
+    };
+    external = true;
+  };
 }

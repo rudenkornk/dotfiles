00a72a03bb722b601000c5c598995ee437dc00c7
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-17 22:30:17 +0300

    feat(neovim): allow C-n and C-p to navigate in insert mode

--- nix/home-manager/text-editors/neovim/config/lua/config/keymaps.lua
+++ nix/home-manager/text-editors/neovim/config/lua/config/keymaps.lua
@@ -151,6 +151,8 @@ vim.keymap.set({ "i" }, "<C-q>", "<C-^>", { desc = "Toggle keyboard layout" })
 
 vim.keymap.set("i", "<C-a>", "<Home>", { desc = "Jump to beginning of line" })
 vim.keymap.set("i", "<C-e>", "<End>", { desc = "Jump to end of line" })
+vim.keymap.set("i", "<C-n>", "<Down>", { desc = "Move cursor down" })
+vim.keymap.set("i", "<C-p>", "<Up>", { desc = "Move cursor up" })
 
 -- Some quality of life mappings.
 vim.keymap.set(

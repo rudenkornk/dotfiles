39d8af6b7a64889b62c30f794f439b5668eb0b51
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-21 17:25:10 +0300

    refactor(virtualization): group home attributes

--- nix/home-manager/virtualization/virtualization.nix
+++ nix/home-manager/virtualization/virtualization.nix
@@ -2,21 +2,23 @@
 
 # Virtualization & containerization tools.
 {
-  home.packages = with pkgs; [
-    docker
-    docker-compose
-    k9s
-    kubectl
-    kubernetes
-    minikube
-    podman
-    qemu_full
-    vagrant
-  ];
+  home = {
+    packages = with pkgs; [
+      docker
+      docker-compose
+      k9s
+      kubectl
+      kubernetes
+      minikube
+      podman
+      qemu_full
+      vagrant
+    ];
 
-  home.shellAliases = {
-    d = "docker";
-    p = "podman";
+    shellAliases = {
+      d = "docker";
+      p = "podman";
+    };
   };
 
   programs.docker-cli = {

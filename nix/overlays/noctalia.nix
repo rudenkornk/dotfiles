597c8b0bb444726aa8843935a76c01d0d11976dc
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-22 18:27:34 +0300

    fix(noctalia): enable fingerprint recovery and D-Bus checks

--- nix/overlays/noctalia.nix
+++ nix/overlays/noctalia.nix
@@ -7,6 +7,16 @@ _: final: prev: {
         url = "https://github.com/noctalia-dev/noctalia/commit/9a84476906165208286399a85fe62d802d47da1a.patch";
         hash = "sha256-wNn66UttKOnqbhARsXGjqJyST1hhRojUL6J1QNdMfGg=";
       })
+      # See https://github.com/noctalia-dev/noctalia/issues/4473
+      (final.locallib.patches + /noctalia-fingerprint-recovery.patch)
+      (final.locallib.patches + /noctalia-upower-test-assertions.patch)
+    ];
+    nativeCheckInputs = (oldAttrs.nativeCheckInputs or [ ]) ++ [
+      (final.writeShellScriptBin "dbus-run-session" ''
+        exec ${final.dbus}/bin/dbus-run-session \
+          --dbus-daemon=${final.dbus}/bin/dbus-daemon \
+          --config-file=${final.dbus}/share/dbus-1/session.conf "$@"
+      '')
     ];
   });
 }

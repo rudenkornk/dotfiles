2c7b3f533cc44534318f5c50211fc8c398c7b8d9
Nikita Rudenko <rudenkornk@gmail.com>
2026-09-23 18:31:21 +0300

    refactor(virtualization): extract system configuration

---
+++ nix/virtualization.nix
@@ -0,0 +1,25 @@
+{ users, ... }:
+
+{
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
+
+  programs.virt-manager.enable = true;
+
+  users.users = builtins.mapAttrs (_name: _user: {
+    extraGroups = [
+      "docker"
+      "libvirtd"
+    ];
+  }) users;
+
+}
